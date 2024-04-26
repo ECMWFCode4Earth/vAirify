9c4a55f89c516846420d6a54cfb018fdb2c7b026
amehta <amehta@scottlogic.com>
2024-04-26 11:31:13 +0100

    Apply multiplication to pollutant values
    Includes refactor

--- Backend/src/cams/forecast_adapter.py
+++ Backend/src/cams/forecast_adapter.py
@@ -1,7 +1,9 @@
-from src.cams.forecast_dao import ForecastData
+from src.cams.forecast_dao import ForecastData, ForecastDataType
+from numpy import ndarray
+from xarray import DataArray
 
 
-def extract_value(data_array, city):
+def find_value_for_city(data_array: DataArray, city) -> ndarray:
     return data_array.sel(
         indexers={'latitude': city["latitude"], 'longitude': city["longitude"]},
         method='nearest'
@@ -9,35 +11,31 @@ def extract_value(data_array, city):
 
 
 def transform(forecast_data: ForecastData, cities):
-    da_pm10 = forecast_data.single_level_data['pm10']
-    da_pm2_5 = forecast_data.single_level_data['pm2p5']
-    da_ozone = forecast_data.multi_level_data['go3']
-    da_no2 = forecast_data.multi_level_data['no2']
-    da_so2 = forecast_data.multi_level_data['so2']
-
-    step_values = forecast_data.single_level_data['step'].values
-    time_value = forecast_data.single_level_data['time'].values
+    step_values = forecast_data.get_step_values()
+    time_value = forecast_data.get_time_value()
     formatted_dataset = []
 
     for city in cities:
-        go3_value = extract_value(da_ozone, city)
-        no2_value = extract_value(da_no2, city)
-        so2_value = extract_value(da_so2, city)
-        pm10_value = extract_value(da_pm10, city)
-        pm2p5_value = extract_value(da_pm2_5, city)
+        city_forecast_data_by_type = {}
         city_name = city["name"]
+
+        for forecast_data_type in ForecastDataType:
+            global_data = forecast_data.get_data(forecast_data_type)
+            pollutant_values_kg_m3 = find_value_for_city(global_data, city).tolist()
+            pollutant_values_ug_m3 = [x * 10**9 for x in pollutant_values_kg_m3]
+            city_forecast_data_by_type[forecast_data_type] = pollutant_values_ug_m3
 
         for i in range(0, step_values.size):
             formatted_dataset.append(
                 {
                     "city": city_name,
                     "city_location": {"type": "Point", "coordinates": [city["longitude"], city["latitude"]]},
-                    "measurement_date": int(time_value) + (float(step_values[i]) * 60 * 60),
-                    "o3": go3_value.tolist()[i],
-                    "no2": no2_value.tolist()[i],
-                    "so2": so2_value.tolist()[i],
-                    "pm10": pm10_value.tolist()[i],
-                    "pm2_5": pm2p5_value.tolist()[i]
+                    "measurement_date": time_value + (float(step_values[i]) * 60 * 60),
+                    "o3": city_forecast_data_by_type[ForecastDataType.OZONE][i],
+                    "no2": city_forecast_data_by_type[ForecastDataType.NITROGEN_DIOXIDE][i],
+                    "so2": city_forecast_data_by_type[ForecastDataType.SULPHUR_DIOXIDE][i],
+                    "pm10": city_forecast_data_by_type[ForecastDataType.PARTICULATE_MATTER_10][i],
+                    "pm2_5": city_forecast_data_by_type[ForecastDataType.PARTICULATE_MATTER_2_5][i]
                 }
             )
 
--- Backend/src/cams/forecast_dao.py
+++ Backend/src/cams/forecast_dao.py
@@ -1,12 +1,45 @@
 import cdsapi
 from datetime import date, timedelta
+from enum import Enum
 import xarray as xr
+
+
+class ForecastDataType(Enum):
+    NITROGEN_DIOXIDE = 'no2'
+    OZONE = 'go3'
+    PARTICULATE_MATTER_2_5 = 'pm2p5'
+    PARTICULATE_MATTER_10 = 'pm10'
+    SULPHUR_DIOXIDE = 'so2'
+
+    def __eq__(self, other):
+        return other.name == self.name and other.value == self.value
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+def _is_single_level(forecast_data_type: ForecastDataType) -> bool:
+    is_pm2_5 = forecast_data_type == ForecastDataType.PARTICULATE_MATTER_2_5
+    is_pm10 = forecast_data_type == ForecastDataType.PARTICULATE_MATTER_10
+    return is_pm10 or is_pm2_5
 
 
 class ForecastData:
     def __init__(self, single_level_data: xr.Dataset, multi_level_data: xr.Dataset):
         self.single_level_data = single_level_data
         self.multi_level_data = multi_level_data
+
+    def get_data(self, forecast_data_type: ForecastDataType) -> xr.DataArray:
+        if _is_single_level(forecast_data_type):
+            return self.single_level_data[forecast_data_type.value]
+        else:
+            return self.multi_level_data[forecast_data_type.value]
+
+    def get_step_values(self):
+        return self.single_level_data['step'].values
+
+    def get_time_value(self) -> int:
+        return int(self.single_level_data['time'].values)
 
 
 def get_base_request_body(model_base_date: str) -> dict:
