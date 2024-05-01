20401d19ab0c61eb4a65f639969a35d0c599f373
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-05-01 15:54:46 +0100

    24 setup eslintprettier in frontend part of repo (#35)
    Co-authored-by: amehta <amehta@scottlogic.com>

--- air-quality-dashboard/e2e/fruit_api.spec.ts
+++ air-quality-dashboard/e2e/fruit_api.spec.ts
@@ -1,26 +1,25 @@
-import { test, expect, type Page } from "@playwright/test";
+import { expect, test } from '@playwright/test';
 
-const correctLink = "https://demo.playwright.dev/api-mocking";
-const incorrectLink = "https://www.google.com/";
-const kiwiBananaImage = "Kiwi-Banana.png";
-const strawberryImage = "Expected-fruit-strawberry.png";
-test("Displays image check ", async ({ page }) => {
+const correctLink = 'https://demo.playwright.dev/api-mocking';
+const kiwiBananaImage = 'Kiwi-Banana.png';
+const strawberryImage = 'Expected-fruit-strawberry.png';
+test('Displays image check ', async ({ page }) => {
   //Api endpoint
-  await page.route("*/**/api/v1/fruits", async (route) => {
+  await page.route('*/**/api/v1/fruits', async (route) => {
     const json = [
       {
-        name: "Kiwi",
+        name: 'Kiwi',
         id: 21,
       },
       {
-        name: "Banana",
+        name: 'Banana',
         id: 21,
       },
     ];
     await route.fulfill({ json });
   });
   //Wait until the header is visible
-  page.getByRole("heading", { name: "Render a List of Fruits" }).isVisible;
+  page.getByRole('heading', { name: 'Render a List of Fruits' }).isVisible;
   await page.goto(correctLink);
   // Expect image to be visible
 
