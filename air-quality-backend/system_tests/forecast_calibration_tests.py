0f374ede3bf67cf5611a414366ebc921d75e9fe9
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-05-30 11:50:30 +0100

    refactor to reduce repetition

--- air-quality-backend/system_tests/forecast_calibration_tests.py
+++ air-quality-backend/system_tests/forecast_calibration_tests.py
@@ -7,49 +7,76 @@ from system_tests.utils.helper_methods import (
     get_ecmwf_forecast_to_dict_for_countries,
     get_database_data,
     get_database_record_by_key_with_datetime,
-    get_ecmwf_record_by_key_with_string,
+)
+
+# Test parameters
+test_city = "Vancouver"
+test_forecast_base_time = datetime(2024, 5, 30, 00, 00, 00)
+test_forecast_valid_time = datetime(2024, 5, 30, 3, 00, 00)
+
+
+load_dotenv(".env-qa")
+
+ecmwf_forecast_file_path = "system_tests/CAMS_surface_concentration_2024053000_V1.csv"
+ecmwf_all = get_ecmwf_forecast_to_dict_for_countries(ecmwf_forecast_file_path)
+ecmwf_vancouver_subset = []
+ecmwf_vancouver_valid_time_subset = []
+
+for entry in ecmwf_all:
+    if entry.get("location_name") == test_city:
+        ecmwf_vancouver_subset.append(entry)
+        if entry.get("valid_time") == test_forecast_valid_time.strftime(
+            "%Y-%m-%dT%H:%M"
+        ):
+            ecmwf_vancouver_valid_time_subset.append(entry)
+
+
+database_vancouver_subset = get_database_data(
+    {
+        "$and": [
+            {"name": test_city},
+            {"forecast_base_time": test_forecast_base_time},
+        ]
+    },
+    "forecast_data",
+    {
+        "_id": 0,
+        "location_type": 0,
+        "source": 0,
+        "location": 0,
+        "overall_aqi_level:": 0,
+    },
+)
+database_vancouver_valid_time_subset = get_database_record_by_key_with_datetime(
+    database_vancouver_subset,
+    "forecast_valid_time",
+    test_forecast_valid_time,
 )
 
 
-def test_vancouver_verify_database_ozone_matches_ecmwf_ozone_forecast():
-    load_dotenv(".env-qa")
-
-    ecmwf_forecast_dict_all = get_ecmwf_forecast_to_dict_for_countries(
-        "system_tests/CAMS_surface_concentration_2024052900_V1.csv"
-    )
-
-    ecmwf_forecast_dict_city_subset = []
-    ecmwf_forecast_dict_city_and_valid_time_subset = []
-
-    for entry in ecmwf_forecast_dict_all:
-        if entry.get("location_name") == "Vancouver":
-            ecmwf_forecast_dict_city_subset.append(entry)
-            if entry.get("valid_time") == "2024-05-29T03:00":
-                ecmwf_forecast_dict_city_and_valid_time_subset.append(entry)
-
-    database_data_dict_city_subset = get_database_data(
-        {
-            "$and": [
-                {"name": "Vancouver"},
-                {"forecast_base_time": datetime(2024, 5, 29, 00, 00, 00)},
-            ]
-        },
-        "forecast_data",
-        {
-            "_id": 0,
-            "location_type": 0,
-            "source": 0,
-            "location": 0,
-            "overall_aqi_level:": 0,
-        },
-    )
-    database_record_with_single_valid_time = get_database_record_by_key_with_datetime(
-        database_data_dict_city_subset,
-        "forecast_valid_time",
-        datetime(2024, 5, 29, 3, 0),
-    )
-
+def test_vancouver_o3():
     assert (
-        database_record_with_single_valid_time["o3_value"]
-        == ecmwf_forecast_dict_city_and_valid_time_subset[0]["O3"]
+        database_vancouver_valid_time_subset["o3_value"]
+        == ecmwf_vancouver_valid_time_subset[0]["O3"]
+    )
+
+
+def test_vancouver_no2():
+    assert (
+        database_vancouver_valid_time_subset["no2_value"]
+        == ecmwf_vancouver_valid_time_subset[0]["NO2"]
+    )
+
+
+def test_vancouver_pm10():
+    assert (
+        database_vancouver_valid_time_subset["pm10_value"]
+        == ecmwf_vancouver_valid_time_subset[0]["PM10"]
+    )
+
+
+def test_vancouver_pm2_5():
+    assert (
+        database_vancouver_valid_time_subset["pm2_5_value"]
+        == ecmwf_vancouver_valid_time_subset[0]["PM2.5"]
     )
