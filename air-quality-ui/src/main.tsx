656ae555e756f45104ac6451d80607fec659b3ba
amehta <amehta@scottlogic.com>
2024-05-30 09:13:07 +0100

    Add react routing

--- air-quality-ui/src/main.tsx
+++ air-quality-ui/src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
