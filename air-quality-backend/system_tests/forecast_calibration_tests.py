463b3961fcac678d893f0f24277c59a76cd19f89
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-05-30 17:01:22 +0100

    expanded tests to more pollutants

--- air-quality-backend/system_tests/forecast_calibration_tests.py
+++ air-quality-backend/system_tests/forecast_calibration_tests.py
@@ -16,6 +16,10 @@ ecmwf_all_data = get_ecmwf_forecast_to_dict_for_countries(ecmwf_forecast_file_pa
 database_all_data = get_database_data("forecast_data")
 allowed_divergence_percentage = 0.1
 
+# Shared test parameters
+test_forecast_base_time = datetime.datetime(2024, 5, 30, 00, 00, 00)
+test_forecast_valid_time = datetime.datetime(2024, 5, 30, 3, 00, 00)
+
 
 @pytest.mark.parametrize(
     "city",
@@ -36,8 +40,6 @@ allowed_divergence_percentage = 0.1
 def test_cities_with_extreme_longitudes_o3(city: str):
     # Test parameters
     test_city = city
-    test_forecast_base_time = datetime.datetime(2024, 5, 30, 00, 00, 00)
-    test_forecast_valid_time = datetime.datetime(2024, 5, 30, 3, 00, 00)
 
     ecmwf_record_for_city_and_valid_time = list(
         filter(
@@ -56,14 +58,164 @@ def test_cities_with_extreme_longitudes_o3(city: str):
         )
     )
 
-    database_ozone_value = database_record_for_city_and_valid_time[0]["o3_value"]
-    ecmwf_forecast_ozone_value = ecmwf_record_for_city_and_valid_time[0]["O3"]
+    database_o3_value = database_record_for_city_and_valid_time[0]["o3_value"]
+    ecmwf_forecast_o3_value = ecmwf_record_for_city_and_valid_time[0]["O3"]
 
     divergence_percentage = calculate_database_divergence_from_ecmwf_forecast_values(
-        database_ozone_value, ecmwf_forecast_ozone_value
+        database_o3_value, ecmwf_forecast_o3_value
     )
     assert (
         divergence_percentage <= allowed_divergence_percentage
     ), "ECMWF forecast: {}, Database value: {}".format(
-        ecmwf_forecast_ozone_value, database_ozone_value
+        ecmwf_forecast_o3_value, database_o3_value
+    )
+
+
+@pytest.mark.parametrize(
+    "city",
+    [
+        "Vancouver",
+        "San Francisco",
+        "Los Angeles",
+        "Abidjan",
+        "Madrid",
+        "Accra",
+        "London",
+        "Barcelona",
+        "Brisbane",
+        "Auckland",
+        "Wellington",
+    ],
+)
+def test_cities_with_extreme_longitudes_no2(city: str):
+    # Test parameters
+    test_city = city
+
+    ecmwf_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["location_name"] == test_city
+            and x["valid_time"] == test_forecast_valid_time.strftime("%Y-%m-%dT%H:%M"),
+            ecmwf_all_data,
+        )
+    )
+
+    database_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["forecast_base_time"] == test_forecast_base_time
+            and x["name"] == test_city
+            and x["forecast_valid_time"] == test_forecast_valid_time,
+            database_all_data,
+        )
+    )
+
+    database_no2_value = database_record_for_city_and_valid_time[0]["no2_value"]
+    ecmwf_forecast_no2_value = ecmwf_record_for_city_and_valid_time[0]["NO2"]
+
+    divergence_percentage = calculate_database_divergence_from_ecmwf_forecast_values(
+        database_no2_value, ecmwf_forecast_no2_value
+    )
+    assert (
+        divergence_percentage <= allowed_divergence_percentage
+    ), "ECMWF forecast: {}, Database value: {}".format(
+        ecmwf_forecast_no2_value, database_no2_value
+    )
+
+
+@pytest.mark.parametrize(
+    "city",
+    [
+        "Vancouver",
+        "San Francisco",
+        "Los Angeles",
+        "Abidjan",
+        "Madrid",
+        "Accra",
+        "London",
+        "Barcelona",
+        "Brisbane",
+        "Auckland",
+        "Wellington",
+    ],
+)
+def test_cities_with_extreme_longitudes_pm10(city: str):
+    # Test parameters
+    test_city = city
+
+    ecmwf_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["location_name"] == test_city
+            and x["valid_time"] == test_forecast_valid_time.strftime("%Y-%m-%dT%H:%M"),
+            ecmwf_all_data,
+        )
+    )
+
+    database_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["forecast_base_time"] == test_forecast_base_time
+            and x["name"] == test_city
+            and x["forecast_valid_time"] == test_forecast_valid_time,
+            database_all_data,
+        )
+    )
+
+    database_pm10_value = database_record_for_city_and_valid_time[0]["pm10_value"]
+    ecmwf_forecast_pm10_value = ecmwf_record_for_city_and_valid_time[0]["PM10"]
+
+    divergence_percentage = calculate_database_divergence_from_ecmwf_forecast_values(
+        database_pm10_value, ecmwf_forecast_pm10_value
+    )
+    assert (
+        divergence_percentage <= allowed_divergence_percentage
+    ), "ECMWF forecast: {}, Database value: {}".format(
+        ecmwf_forecast_pm10_value, database_pm10_value
+    )
+
+
+@pytest.mark.parametrize(
+    "city",
+    [
+        "Vancouver",
+        "San Francisco",
+        "Los Angeles",
+        "Abidjan",
+        "Madrid",
+        "Accra",
+        "London",
+        "Barcelona",
+        "Brisbane",
+        "Auckland",
+        "Wellington",
+    ],
+)
+def test_cities_with_extreme_longitudes_pm2_5(city: str):
+    # Test parameters
+    test_city = city
+
+    ecmwf_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["location_name"] == test_city
+            and x["valid_time"] == test_forecast_valid_time.strftime("%Y-%m-%dT%H:%M"),
+            ecmwf_all_data,
+        )
+    )
+
+    database_record_for_city_and_valid_time = list(
+        filter(
+            lambda x: x["forecast_base_time"] == test_forecast_base_time
+            and x["name"] == test_city
+            and x["forecast_valid_time"] == test_forecast_valid_time,
+            database_all_data,
+        )
+    )
+
+    database_pm2_5_value = database_record_for_city_and_valid_time[0]["pm2_5_value"]
+    ecmwf_forecast_pm2_5_value = ecmwf_record_for_city_and_valid_time[0]["PM2.5"]
+
+    divergence_percentage = calculate_database_divergence_from_ecmwf_forecast_values(
+        database_pm2_5_value, ecmwf_forecast_pm2_5_value
+    )
+    assert (
+        divergence_percentage <= allowed_divergence_percentage
+    ), "ECMWF forecast: {}, Database value: {}".format(
+        ecmwf_forecast_pm2_5_value, database_pm2_5_value
     )
