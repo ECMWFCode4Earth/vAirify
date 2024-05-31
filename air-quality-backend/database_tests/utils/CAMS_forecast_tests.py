e3136f38d8046a7110a46b0ff4c6aaa490c812de
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-05-31 14:02:29 +0100

    Condense test
    each pollutant into an array, then loop through it and assert in each

--- air-quality-backend/database_tests/utils/CAMS_forecast_tests.py
+++ air-quality-backend/database_tests/utils/CAMS_forecast_tests.py
@@ -18,26 +18,16 @@ def test_aqi_levels_are_between_1_and_6():
     collection_name = "forecast_data"
 
     dict_result = get_database_data(query, collection_name)
-    pprint.pprint(dict_result)
+
+    pollutant_keys = ["no2_aqi_level", "so2_aqi_level", "o3_aqi_level", "pm10_aqi_level", "pm2_5_aqi_level"]
 
     for document in dict_result:
         overall_aqi_level = document["overall_aqi_level"]
         assert 1 <= overall_aqi_level <= 6, f"overall_aqi_level {overall_aqi_level} is out of range"
 
-        no2_aqi_level = document["no2_aqi_level"]
-        assert 1 <= no2_aqi_level <= 6, f"individual_aqi_level {no2_aqi_level} is out of range"
-
-        o3_aqi_level = document["o3_aqi_level"]
-        assert 1 <= o3_aqi_level <= 6, f"individual_aqi_level {o3_aqi_level} is out of range"
-
-        so2_aqi_level = document["so2_aqi_level"]
-        assert 1 <= so2_aqi_level <= 6, f"individual_aqi_level {so2_aqi_level} is out of range"
-
-        pm10_aqi_level = document["pm10_aqi_level"]
-        assert 1 <= pm10_aqi_level <= 6, f"individual_aqi_level {pm10_aqi_level} is out of range"
-
-        pm2_5_aqi_level = document["pm2_5_aqi_level"]
-        assert 1 <= pm2_5_aqi_level <= 6, f"individual_aqi_level {pm2_5_aqi_level} is out of range"
+    for document in dict_result:
+        for key in pollutant_keys:
+            assert 1 <= document[key] <= 6, f"{key} {document[key]} is out of range"
 
 
 def dict_print():
