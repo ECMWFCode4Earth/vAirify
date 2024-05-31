6ba26523aba50a5d5b942da5fe105d0ded0ecce4
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-05-31 13:28:47 +0100

    Squashed aqi_level tests
    Condense repetitive tests

--- air-quality-backend/database_tests/utils/db_retrieve.py
+++ air-quality-backend/database_tests/utils/db_retrieve.py
@@ -7,10 +7,9 @@ allowed_aqi_indexes = {1, 2, 3, 4, 5, 6}
 overall_aqi_value_key = "overall_aqi_level"
 location_type_key = "location_type"
 allowed_location_type = "city"
-individual_pollutant_aqi = "no2:aqi_level"
 
 
-def test_overall_aqi_level_is_between_1_and_6():
+def test_aqi_levels_are_between_1_and_6():
     import os
     os.environ["MONGO_DB_URI"] = "mongodb+srv://mnyamunda:CVbP4nSZWfDtEAzT@cluster0.ch5gkk4.mongodb.net/"
     os.environ["MONGO_DB_NAME"] = "air_quality_dashboard_db_max"
@@ -24,6 +23,21 @@ def test_overall_aqi_level_is_between_1_and_6():
     for document in dict_result:
         overall_aqi_level = document["overall_aqi_level"]
         assert 1 <= overall_aqi_level <= 6, f"overall_aqi_level {overall_aqi_level} is out of range"
+
+        no2_aqi_level = document["no2_aqi_level"]
+        assert 1 <= no2_aqi_level <= 6, f"individual_aqi_level {no2_aqi_level} is out of range"
+
+        o3_aqi_level = document["o3_aqi_level"]
+        assert 1 <= o3_aqi_level <= 6, f"individual_aqi_level {o3_aqi_level} is out of range"
+
+        so2_aqi_level = document["so2_aqi_level"]
+        assert 1 <= so2_aqi_level <= 6, f"individual_aqi_level {so2_aqi_level} is out of range"
+
+        pm10_aqi_level = document["pm10_aqi_level"]
+        assert 1 <= pm10_aqi_level <= 6, f"individual_aqi_level {pm10_aqi_level} is out of range"
+
+        pm2_5_aqi_level = document["pm2_5_aqi_level"]
+        assert 1 <= pm2_5_aqi_level <= 6, f"individual_aqi_level {pm2_5_aqi_level} is out of range"
 
 
 def dict_print():
