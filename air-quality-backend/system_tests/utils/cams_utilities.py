7dacb76cb343d27d5dab956bc8141dc97e31ca8a
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-06-04 09:46:30 +0100

    33 Create automation for previously tested tickets. (#68)
    * Basic Database pull and assert
    
    * Changed name of key
    
    * Added location type assertion
    to only allow city as a value
    
    * minor comment updates
    
    * minor comment updates
    
    * Document count to ensure all files
    are asserted upon
    
    * Create db_helpers.py
    
    Helper method to create dictionary
    
    * Refactor fetch method
    
    request made and stored through helper function as opposed to in file
    
    * Squashed aqi_level tests
    
    Condense repetitive tests
    
    * Condense test
    
    each pollutant into an array, then loop through it and assert in each
    
    * Clean up repetition
    
    code reduction
    
    * location_type check
    
    checks that location type must be a city, changed to get credentials from .env as opposed to raw text input
    
    * linting
    
    Cleanup
    
    * Test separation
    
    separate overall aqi and individual aqi checks
    
    * Open query
    
    * query refactor
    
    user can enter query directly into parameter parentheses as opposed to an outside variable.
    
    * Directory change
    
    Moved test files, updated get_database_data helper method, delete old folders
    
    * Lint and format
    
    Formatting files to resolve pipeline failure

--- air-quality-backend/system_tests/utils/cams_utilities.py
+++ air-quality-backend/system_tests/utils/cams_utilities.py
@@ -5,6 +5,7 @@ import xarray
 from decimal import Decimal
 from pandas import read_csv
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure, OperationFailure
 
 
 def get_dataset_from_coordinates_many_steps(
@@ -102,30 +103,50 @@ def get_raw_cams_data(
         )
 
 
-def get_database_data(collection_name: str):
+def get_database_data(query: dict, collection_name: str):
     uri = os.environ.get("MONGO_DB_URI")
     db_name = os.environ.get("MONGO_DB_NAME")
-    client = MongoClient(uri)
-    collection = client[db_name][collection_name]
-    cursor = collection.find()
-    database_dictionary_list = []
 
-    for document in cursor:
-        document_as_dict = {
-            "name": document["name"],
-            "created_time": document["created_time"],
-            "last_modified_time": document["last_modified_time"],
-            "forecast_base_time": document["forecast_base_time"],
-            "forecast_range": document["forecast_range"],
-            "forecast_valid_time": document["forecast_valid_time"],
-            "o3_value": document["o3"]["value"],
-            "so2_value": document["so2"]["value"],
-            "no2_value": document["no2"]["value"],
-            "pm10_value": document["pm10"]["value"],
-            "pm2_5_value": document["pm2_5"]["value"],
-        }
-        database_dictionary_list.append(document_as_dict)
-    client.close()
+    if not uri or not db_name:
+        raise ValueError("MONGO_DB_URI and MONGO_DB_NAME must be set!")
+
+    try:
+        client = MongoClient(uri)
+        collection = client[db_name][collection_name]
+        cursor = collection.find(query)
+        database_dictionary_list = []
+
+        for document in cursor:
+            document_as_dict = {
+                "name": document["name"],
+                "created_time": document["created_time"],
+                "last_modified_time": document["last_modified_time"],
+                "forecast_base_time": document["forecast_base_time"],
+                "forecast_range": document["forecast_range"],
+                "forecast_valid_time": document["forecast_valid_time"],
+                "overall_aqi_level": document["overall_aqi_level"],
+                "location_type": document["location_type"],
+                "o3_value": document["o3"]["value"],
+                "so2_value": document["so2"]["value"],
+                "no2_value": document["no2"]["value"],
+                "pm10_value": document["pm10"]["value"],
+                "pm2_5_value": document["pm2_5"]["value"],
+                "o3_aqi_level": document["o3"]["aqi_level"],
+                "so2_aqi_level": document["so2"]["aqi_level"],
+                "no2_aqi_level": document["no2"]["aqi_level"],
+                "pm10_aqi_level": document["pm10"]["aqi_level"],
+                "pm2_5_aqi_level": document["pm2_5"]["aqi_level"],
+            }
+            database_dictionary_list.append(document_as_dict)
+
+    except ConnectionFailure:
+        raise RuntimeError("Failed to connect to the database")
+    except OperationFailure as e:
+        raise RuntimeError(f"Database operation failed: {e}")
+
+    finally:
+        client.close()
+
     return database_dictionary_list
 
 
