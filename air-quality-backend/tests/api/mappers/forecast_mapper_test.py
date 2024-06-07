60a933115869b8babe28ff20ae0059c34dba63ec
amehta <amehta@scottlogic.com>
2024-06-07 12:21:00 +0100

    Review comment

--- air-quality-backend/tests/api/mappers/forecast_mapper_test.py
+++ air-quality-backend/tests/api/mappers/forecast_mapper_test.py
@@ -5,18 +5,24 @@ from tests.util.mock_forecast_data import create_mock_forecast_document
 
 
 def test__map_forecast_database_data_to_api_output_data():
-    result = map_forecast([create_mock_forecast_document({})])
+    result = map_forecast(
+        [
+            create_mock_forecast_document({"name": "123"}),
+            create_mock_forecast_document({"name": "456"}),
+        ]
+    )
+    expected = {
+        "base_time": datetime(2024, 5, 27, 12, 0, tzinfo=timezone.utc),
+        "location_type": "city",
+        "overall_aqi_level": 1,
+        "o3": {"aqi_level": 1, "value": 1.0},
+        "no2": {"aqi_level": 1, "value": 2.0},
+        "so2": {"aqi_level": 1, "value": 3.0},
+        "pm10": {"aqi_level": 2, "value": 4.0},
+        "pm2_5": {"aqi_level": 2, "value": 5.0},
+        "valid_date": datetime(2024, 5, 27, 12, 0, tzinfo=timezone.utc),
+    }
     assert result == [
-        {
-            "base_time": datetime(2024, 5, 27, 12, 0, tzinfo=timezone.utc),
-            "location_name": "Abidjan",
-            "location_type": "city",
-            "overall_aqi_level": 1,
-            "o3": {"aqi_level": 1, "value": 1.0},
-            "no2": {"aqi_level": 1, "value": 2.0},
-            "so2": {"aqi_level": 1, "value": 3.0},
-            "pm10": {"aqi_level": 2, "value": 4.0},
-            "pm2_5": {"aqi_level": 2, "value": 5.0},
-            "valid_date": datetime(2024, 5, 27, 12, 0, tzinfo=timezone.utc),
-        }
+        {**expected, "location_name": "123"},
+        {**expected, "location_name": "456"},
     ]
