e9d05ce95a854d181db6b6c643a202beffb05509
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-06-10 16:07:36 +0100

    seeding database

--- air-quality-backend/system_tests/api_suite/forecast_api_tests.py
+++ air-quality-backend/system_tests/api_suite/forecast_api_tests.py
@@ -1,14 +1,19 @@
 import datetime
-import pprint
 import random
-
 import pytest
 import requests
+from dotenv import load_dotenv
 
+from air_quality.database.forecasts import Forecast
 from system_tests.utils.api_utilities import (
     format_datetime_as_string,
     create_forecast_api_parameters_payload,
+    get_expected_valid_times_list,
+    get_list_of_keys,
+    seed_api_test_data,
+    get_forecast,
 )
+from system_tests.utils.cams_utilities import delete_database_data
 from system_tests.utils.cities_data import all_cities
 
 base_url = "http://127.0.0.1:8000/air-pollutant/forecast"
@@ -16,14 +21,14 @@ headers = {"accept": "application/json"}
 
 location_type = "city"
 location_name = random.choice(all_cities)
-forecast_base_time = datetime.datetime(2024, 6, 6, 0, 0)
+forecast_base_time = datetime.datetime(2024, 6, 10, 0, 0)
 forecast_base_time_string = format_datetime_as_string(
     forecast_base_time,
     "%Y-%m-%dT%H:%M:%S+00:00",
 )
 valid_date_from = forecast_base_time_string
 valid_date_to = format_datetime_as_string(
-    datetime.datetime(2024, 6, 11, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
+    datetime.datetime(2024, 6, 15, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
 )
 
 success_status_code = 200
@@ -120,30 +125,29 @@ def test__required_and_optional_parameter_combinations__verify_response_status_c
 
 
 def test__location_name_parameter__returns_41_results():
-    payload = create_forecast_api_parameters_payload(
+    response = get_forecast(
         forecast_base_time_string,
         valid_date_from,
         valid_date_to,
         location_type,
         location_name,
+        base_url,
+        headers,
     )
-    response = requests.request(
-        "GET", base_url, headers=headers, params=payload, timeout=5.0
-    )
+
     response_json = response.json()
     assert len(response_json) == 41
 
 
 def test__location_name_parameter__returns_results_containing_correct_location_name():
-    payload = create_forecast_api_parameters_payload(
+    response = get_forecast(
         forecast_base_time_string,
         valid_date_from,
         valid_date_to,
         location_type,
         location_name,
-    )
-    response = requests.request(
-        "GET", base_url, headers=headers, params=payload, timeout=5.0
+        base_url,
+        headers,
     )
     response_json = response.json()
     for forecast in response_json:
@@ -151,50 +155,71 @@ def test__location_name_parameter__returns_results_containing_correct_location_n
 
 
 def test__location_name_parameter__returns_results_containing_correct_base_time():
-    payload = create_forecast_api_parameters_payload(
+    response = get_forecast(
         forecast_base_time_string,
         valid_date_from,
         valid_date_to,
         location_type,
         location_name,
-    )
-    response = requests.request(
-        "GET", base_url, headers=headers, params=payload, timeout=5.0
+        base_url,
+        headers,
     )
     response_json = response.json()
     for forecast in response_json:
-        assert forecast["base_time"] == forecast_base_time
+        assert forecast["base_time"] == forecast_base_time_string
 
 
 def test__location_name_parameter__returns_results_containing_correct_valid_times():
-    payload = create_forecast_api_parameters_payload(
+    response = get_forecast(
         forecast_base_time_string,
         valid_date_from,
         valid_date_to,
         location_type,
         location_name,
-    )
-    response = requests.request(
-        "GET", base_url, headers=headers, params=payload, timeout=5.0
+        base_url,
+        headers,
     )
     response_json = response.json()
+    expected_valid_time_list = get_expected_valid_times_list(forecast_base_time, 3)
+    actual_valid_time_list = get_list_of_keys(response_json, "valid_date")
 
-    actual_valid_time_list = []
-    for forecast in response_json:
-        actual_valid_time_list.append(forecast["valid_date"])
-    # print(actual_valid_time_list)
+    assert expected_valid_time_list == actual_valid_time_list
 
-    expected_valid_time_list = [forecast_base_time]
-    step = datetime.timedelta(hours=3)
 
-    i = 1
-    while i < 42:
-        next_valid_time = (
-            expected_valid_time_list[len(expected_valid_time_list) - 1] + step
-        )
-        expected_valid_time_list.append(next_valid_time)
-        i += 1
+def test__verify__parameters__work():
+    load_dotenv(".env-qa")
+    delete_database_data("forecast_data")
 
-    sorted_expected_valid_time_list = expected_valid_time_list.sort()
-    sorted_actual_valid_time_list = actual_valid_time_list.sort()
-    assert sorted_expected_valid_time_list == sorted_actual_valid_time_list
+    test_forecast_base_time = datetime.datetime(2024, 6, 9, 12, 0, 0)
+
+    forecast: Forecast = {
+        "forecast_valid_time": datetime.datetime(2024, 6, 9, 12, 0, 0),
+        "source": "cams-production",
+        "forecast_base_time": test_forecast_base_time,
+        "location_type": "city",
+        "name": "Test City 1",
+        "forecast_range": 0,
+        "last_modified_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
+        "location": {
+            "type": "Point",
+            "coordinates": [54.36667, 24.46667],
+        },
+        "no2": {"aqi_level": 1, "value": 7.79346375328925},
+        "o3": {"aqi_level": 4, "value": 212.70172151472397},
+        "overall_aqi_level": 6,
+        "pm10": {"aqi_level": 6, "value": 205.640266314635},
+        "pm2_5": {"aqi_level": 4, "value": 48.76003397454627},
+        "so2": {"aqi_level": 1, "value": 7.58745619326088},
+        "created_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
+    }
+
+    seed_api_test_data("forecast_data", [forecast])
+    get_forecast(
+        forecast_base_time_string,
+        valid_date_from,
+        valid_date_to,
+        location_type,
+        location_name,
+        base_url,
+        headers,
+    )
--- air-quality-backend/system_tests/utils/api_utilities.py
+++ air-quality-backend/system_tests/utils/api_utilities.py
@@ -1,4 +1,8 @@
 import datetime
+import os
+import requests
+from pymongo import MongoClient
+from air_quality.database.forecasts import Forecast
 
 
 def format_datetime_as_string(date_and_time: datetime, string_format: str) -> str:
@@ -41,3 +45,65 @@ def check_for_and_remove_unused_parameters(
         if api_parameter_payload[parameter] == "":
             del api_parameter_payload[parameter]
     return api_parameter_payload
+
+
+def get_forecast(
+    forecast_base_time_string: str,
+    valid_date_from: str,
+    valid_date_to: str,
+    location_type: str,
+    location_name: str,
+    base_url: str,
+    headers: dict,
+):
+    payload = create_forecast_api_parameters_payload(
+        forecast_base_time_string,
+        valid_date_from,
+        valid_date_to,
+        location_type,
+        location_name,
+    )
+    return requests.request(
+        "GET", base_url, headers=headers, params=payload, timeout=5.0
+    )
+
+
+def get_expected_valid_times_list(
+    forecast_base_time: datetime.datetime, step_hours: int
+) -> list[str]:
+    expected_valid_time_list = [forecast_base_time]
+    step = datetime.timedelta(hours=step_hours)
+    number_of_forecast_times = ((24 / step_hours) * 5) + 1
+
+    i = 1
+    while i <= (number_of_forecast_times - 1):
+        next_valid_time = (
+            expected_valid_time_list[len(expected_valid_time_list) - 1] + step
+        )
+        expected_valid_time_list.append(next_valid_time)
+        i += 1
+
+    formatted_expected_valid_time_list = []
+
+    for valid_time in expected_valid_time_list:
+        formatted_valid_time = format_datetime_as_string(
+            valid_time, "%Y-%m-%dT%H:%M:%S+00:00"
+        )
+        formatted_expected_valid_time_list.append(formatted_valid_time)
+
+    return formatted_expected_valid_time_list
+
+
+def get_list_of_keys(response_json, key_to_add_to_list: str) -> list:
+    list_of_keys = []
+    for forecast in response_json:
+        list_of_keys.append(forecast[key_to_add_to_list])
+    return list_of_keys
+
+
+def seed_api_test_data(collection_name: str, test_data_list: list[Forecast]):
+    uri = os.environ.get("MONGO_DB_URI")
+    db_name = os.environ.get("MONGO_DB_NAME")
+    client = MongoClient(uri)
+    collection = client[db_name][collection_name]
+    collection.insert_many(test_data_list)
