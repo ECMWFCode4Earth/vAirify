7819d82669ee556b6abb83ae5a844a53d1372cc3
rstrange-scottlogic <rstrange@scottlogic.com>
2024-06-11 11:18:26 +0100

    Fixes following merge

--- air-quality-backend/tests/aqi/pollutant_type_test.py
+++ air-quality-backend/tests/aqi/pollutant_type_test.py
@@ -59,38 +59,3 @@ def test_pollutants_with_molecular_weight():
     result = pollutants_with_molecular_weight()
 
     assert expected == result
-
-
-@pytest.mark.parametrize(
-    "pollutant_type, expected",
-    [
-        (PollutantType.NITROGEN_DIOXIDE, 46.01),
-        (PollutantType.SULPHUR_DIOXIDE, 64.07),
-        (PollutantType.OZONE, 48),
-    ],
-)
-def test_get_molecular_weight_valid(pollutant_type: PollutantType, expected: float):
-    assert get_molecular_weight(pollutant_type) == expected
-
-
-@pytest.mark.parametrize(
-    "pollutant_type",
-    [
-        PollutantType.PARTICULATE_MATTER_2_5,
-        PollutantType.PARTICULATE_MATTER_10,
-    ],
-)
-def test_get_molecular_weight_invalid(pollutant_type: PollutantType):
-    with pytest.raises(ValueError):
-        get_molecular_weight(pollutant_type)
-
-
-def test_pollutants_with_molecular_weight():
-    expected = [
-        PollutantType.NITROGEN_DIOXIDE,
-        PollutantType.SULPHUR_DIOXIDE,
-        PollutantType.OZONE,
-    ]
-    result = pollutants_with_molecular_weight()
-
-    assert expected == result
