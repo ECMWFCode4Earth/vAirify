548af630dc71aff7a700d2ddcc0e0c37d1b0e7c7
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-06-11 10:50:41 +0100

    parameterized test checking values

--- air-quality-backend/system_tests/api_suite/forecast_api_seeded_data_tests.py
+++ air-quality-backend/system_tests/api_suite/forecast_api_seeded_data_tests.py
@@ -1,7 +1,10 @@
 import datetime
+import json
+import pprint
 
 import pytest
 
+from air_quality.api.types import ForecastDto
 from air_quality.database.forecasts import Forecast
 from system_tests.utils.api_utilities import (
     format_datetime_as_string,
@@ -11,7 +14,7 @@ from system_tests.utils.api_utilities import (
 )
 
 # Test Data
-test_city_1_data: Forecast = {
+test_city_1_input_data: Forecast = {
     "forecast_valid_time": datetime.datetime(2024, 6, 10, 3, 0, 0),
     "source": "cams-production",
     "forecast_base_time": datetime.datetime(2024, 6, 10, 0, 0, 0),
@@ -32,7 +35,7 @@ test_city_1_data: Forecast = {
     "created_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
 }
 
-test_city_2_data: Forecast = {
+test_city_2_input_data: Forecast = {
     "forecast_valid_time": datetime.datetime(2024, 6, 12, 0, 0, 0),
     "source": "cams-production",
     "forecast_base_time": datetime.datetime(2024, 6, 10, 0, 0, 0),
@@ -44,16 +47,16 @@ test_city_2_data: Forecast = {
         "type": "Point",
         "coordinates": [54.36667, 24.46667],
     },
-    "no2": {"aqi_level": 1, "value": 7.79346375328925},
-    "o3": {"aqi_level": 4, "value": 212.70172151472397},
+    "no2": {"aqi_level": 6, "value": 350.76859403417895},
+    "o3": {"aqi_level": 5, "value": 261.70172151472397},
     "overall_aqi_level": 6,
-    "pm10": {"aqi_level": 6, "value": 205.640266314635},
-    "pm2_5": {"aqi_level": 4, "value": 48.76003397454627},
-    "so2": {"aqi_level": 1, "value": 7.58745619326088},
+    "pm10": {"aqi_level": 1, "value": 10.640266314635},
+    "pm2_5": {"aqi_level": 1, "value": 9.76003397454627},
+    "so2": {"aqi_level": 3, "value": 212.58745619326088},
     "created_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
 }
 
-test_city_3_data: Forecast = {
+test_city_3_input_data: Forecast = {
     "forecast_valid_time": datetime.datetime(2024, 6, 10, 15, 0, 0),
     "source": "cams-production",
     "forecast_base_time": datetime.datetime(2024, 6, 11, 0, 0, 0),
@@ -65,13 +68,48 @@ test_city_3_data: Forecast = {
         "type": "Point",
         "coordinates": [54.36667, 24.46667],
     },
+    "no2": {"aqi_level": 4, "value": 150.79346375328925},
+    "o3": {"aqi_level": 3, "value": 101.70172151472397},
+    "overall_aqi_level": 4,
+    "pm10": {"aqi_level": 2, "value": 30.640266314635},
+    "pm2_5": {"aqi_level": 3, "value": 22.76003397454627},
+    "so2": {"aqi_level": 2, "value": 103.58745619326088},
+    "created_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
+}
+
+test_city_1_response_data: dict = {
+    "base_time": format_datetime_as_string(
+        datetime.datetime(2024, 6, 10, 0, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
+    ),
+    "valid_time": format_datetime_as_string(
+        datetime.datetime(2024, 6, 10, 3, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
+    ),
+    "location_type": "city",
+    "location_name": "Test City 1",
+    "overall_aqi_level": 6,
     "no2": {"aqi_level": 1, "value": 7.79346375328925},
     "o3": {"aqi_level": 4, "value": 212.70172151472397},
-    "overall_aqi_level": 6,
-    "pm10": {"aqi_level": 6, "value": 205.640266314635},
     "pm2_5": {"aqi_level": 4, "value": 48.76003397454627},
+    "pm10": {"aqi_level": 6, "value": 205.640266314635},
     "so2": {"aqi_level": 1, "value": 7.58745619326088},
-    "created_time": datetime.datetime(2024, 6, 10, 7, 49, 53, 664),
+}
+
+
+test_city_2_response_data: dict = {
+    "base_time": format_datetime_as_string(
+        datetime.datetime(2024, 6, 10, 0, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
+    ),
+    "valid_time": format_datetime_as_string(
+        datetime.datetime(2024, 6, 12, 0, 0, 0), "%Y-%m-%dT%H:%M:%S+00:00"
+    ),
+    "location_type": "city",
+    "location_name": "Test City 2",
+    "overall_aqi_level": 6,
+    "no2": {"aqi_level": 6, "value": 350.76859403417895},
+    "o3": {"aqi_level": 5, "value": 261.70172151472397},
+    "pm2_5": {"aqi_level": 1, "value": 9.76003397454627},
+    "pm10": {"aqi_level": 1, "value": 10.640266314635},
+    "so2": {"aqi_level": 3, "value": 212.58745619326088},
 }
 
 # API GET request
@@ -91,7 +129,9 @@ valid_time_to = format_datetime_as_string(
 
 # Test Setup
 setup_purge_database_and_seed_with_test_data(
-    ".env-qa", "forecast_data", [test_city_1_data, test_city_2_data, test_city_3_data]
+    ".env-qa",
+    "forecast_data",
+    [test_city_1_input_data, test_city_2_input_data, test_city_3_input_data],
 )
 
 
@@ -296,16 +336,16 @@ def test__valid_time_to_bva__assert_number_of_results(
     assert len(response_json) == expected
 
 
-@pytest.mark.parametrize("location_name", ("", "Test City 1"))
+@pytest.mark.parametrize("test_location_name", ("", "Test City 1"))
 def test__results_containing_relevant_base_time(
-    location_name: str,
+    test_location_name: str,
 ):
     response = get_forecast(
         base_time_string,
         valid_time_from,
         valid_time_to,
         location_type,
-        location_name,
+        test_location_name,
         base_url,
         headers,
     )
@@ -329,3 +369,39 @@ def test__results_containing_relevant_base_time(
 #     actual_valid_time_list = get_list_of_keys(response_json, "valid_date")
 #
 #     assert expected_valid_time_list == actual_valid_time_list
+
+
+@pytest.mark.parametrize(
+    "test_location_name, expected_response",
+    [
+        (
+            "",
+            [
+                test_city_1_response_data,
+                test_city_2_response_data,
+            ],
+        ),
+        (
+            "Test City 2",
+            [
+                test_city_2_response_data,
+            ],
+        ),
+    ],
+)
+def test__assert_response_keys_and_values_are_correct(
+    test_location_name: str, expected_response: list
+):
+    pprint.pprint(expected_response)
+    response = get_forecast(
+        base_time_string,
+        valid_time_from,
+        valid_time_to,
+        location_type,
+        test_location_name,
+        base_url,
+        headers,
+    )
+    response_json = response.json()
+    pprint.pprint(response_json)
+    assert response_json == expected_response
