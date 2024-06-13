f35e48d3dcbbde920d935f4198c6f7cfeb582ba9
rstrange-scottlogic <rstrange@scottlogic.com>
2024-06-13 10:46:03 +0100

    Parameterised the initial forecast base date when fetching forecast data. Also updated to properly account for same day and previous day requests

--- air-quality-backend/tests/scripts/run_forecast_etl_test.py
+++ air-quality-backend/tests/scripts/run_forecast_etl_test.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timezone
 from unittest.mock import patch
+
+from freezegun import freeze_time
 
 from air_quality.database.locations import AirQualityLocationType
 from air_quality.etl.forecast.forecast_data import ForecastData
@@ -16,15 +19,47 @@ transformed_forecast_data = []
 @patch("scripts.run_forecast_etl.transform")
 @patch("scripts.run_forecast_etl.fetch_forecast_data")
 @patch("scripts.run_forecast_etl.get_locations_by_type")
-def test__run_forecast_etl(mock_locations, mock_fetch, mock_transform, mock_insert):
+@patch("os.environ.get")
+@freeze_time("2024-05-24T11:12:13")
+def test__run_forecast_etl__no_forecast_time_in_environment_uses_now(
+    mock_os, mock_locations, mock_fetch, mock_transform, mock_insert
+):
     with patch("scripts.run_forecast_etl.config.fileConfig"):
+        mock_os.return_value = None
         mock_locations.return_value = cities
         mock_fetch.return_value = forecast_data
         mock_transform.return_value = transformed_forecast_data
 
         main()
 
+        expected_datetime = datetime(2024, 5, 24, 11, 12, 13, 0, timezone.utc)
+        mock_os.assert_called_with("FORECAST_INITIAL_DATE_TIME")
         mock_locations.assert_called_with(AirQualityLocationType.CITY)
-        mock_fetch.assert_called()
+        mock_fetch.assert_called_with(expected_datetime)
+        mock_transform.assert_called_with(forecast_data, cities)
+        mock_insert.assert_called_with(transformed_forecast_data)
+
+
+@patch("scripts.run_forecast_etl.insert_data")
+@patch("scripts.run_forecast_etl.transform")
+@patch("scripts.run_forecast_etl.fetch_forecast_data")
+@patch("scripts.run_forecast_etl.get_locations_by_type")
+@patch("os.environ.get")
+@freeze_time("2024-05-24T11:12:13")
+def test__run_forecast_etl__forecast_time_in_environment_uses(
+    mock_os, mock_locations, mock_fetch, mock_transform, mock_insert
+):
+    with patch("scripts.run_forecast_etl.config.fileConfig"):
+        mock_os.return_value = "2024-06-01 17"
+        mock_locations.return_value = cities
+        mock_fetch.return_value = forecast_data
+        mock_transform.return_value = transformed_forecast_data
+
+        main()
+
+        expected_datetime = datetime(2024, 6, 1, 17, 0, 0, 0)
+        mock_os.assert_called_with("FORECAST_INITIAL_DATE_TIME")
+        mock_locations.assert_called_with(AirQualityLocationType.CITY)
+        mock_fetch.assert_called_with(expected_datetime)
         mock_transform.assert_called_with(forecast_data, cities)
         mock_insert.assert_called_with(transformed_forecast_data)
