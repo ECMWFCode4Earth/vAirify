21a260c6593860647167de7edb4bc55ec4dd0cb2
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-06-13 15:38:47 +0100

    Extra parameters, remove unused file
    Add extra paras to our test, Add a created time test

--- air-quality-backend/system_tests/forecast_etl_suite/cams_known_grib_test.py
+++ air-quality-backend/system_tests/forecast_etl_suite/cams_known_grib_test.py
@@ -1,4 +1,4 @@
-from datetime import timezone, datetime
+from datetime import timezone, datetime, timedelta
 import pytest
 from system_tests.utils.cams_utilities import delete_database_data
 from scripts.run_forecast_etl import main
@@ -10,6 +10,8 @@ from unittest import mock
 load_dotenv()
 
 test_cases = [
+
+    # Close to -180 Longitude
     (
         {
             "name": "Vancouver",
@@ -33,6 +35,57 @@ test_cases = [
             "pm2_5_value": 3.6327574082026914,
             "pm2_5_aqi_level": 1,
         },
+
+    ),
+    # Extreme Longitude Check 180
+    (
+        {
+            "name": "Wellington",
+            "forecast_valid_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
+            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
+            "location_type": "city",
+            "source": "cams-production",
+        },
+        {
+            "name": "Wellington",
+            "forecast_valid_time": datetime(2024, 6, 4, 0, 0, 0),
+            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0),
+            "no2_value": 0.4782831135385457,
+            "no2_aqi_level": 1,
+            "o3_value": 56.25565199915405,
+            "o3_aqi_level": 2,
+            "so2_value": 0.3777866472717213,
+            "so2_aqi_level": 1,
+            "pm10_value": 16.71105682154486,
+            "pm10_aqi_level": 1,
+            "pm2_5_value": 9.073147098209494,
+            "pm2_5_aqi_level": 1,
+        },
+    ),
+    # Close to 0 longitude
+    (
+        {
+            "name": "London",
+            "forecast_valid_time": datetime(2024, 6, 4, 6, 0, 0, tzinfo=timezone.utc),
+            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
+            "location_type": "city",
+            "source": "cams-production",
+        },
+        {
+            "name": "London",
+            "forecast_valid_time": datetime(2024, 6, 4, 6, 0, 0),
+            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0),
+            "no2_value": 19.136068918675015,
+            "no2_aqi_level": 1,
+            "o3_value": 9.499161050717573,
+            "o3_aqi_level": 1,
+            "so2_value": 3.8678773476028074,
+            "so2_aqi_level": 1,
+            "pm10_value": 24.70699837214256,
+            "pm10_aqi_level": 2,
+            "pm2_5_value": 17.1565041916872,
+            "pm2_5_aqi_level": 2,
+        },
     ),
 ]
 
@@ -44,7 +97,7 @@ test_cases = [
     },
 )
 @pytest.mark.parametrize("query_params, expected_values", test_cases)
-def test_known_vancouver_grib(query_params, expected_values):
+def test__extreme_longitudes__to_assert_exact_values(query_params, expected_values):
     delete_database_data("forecast_data")
     main()
 
@@ -71,23 +124,38 @@ def test_known_vancouver_grib(query_params, expected_values):
                 document["no2_value"] == expected_values["no2_value"]
         ), "no2 value does not match!"
         assert (
+                document["no2_aqi_level"] == expected_values["no2_aqi_level"]
+        ), "no2 aqi value does not match!"
+        assert (
                 document["o3_value"] == expected_values["o3_value"]
         ), "o3 value does not match!"
+        assert (
+                document["o3_aqi_level"] == expected_values["o3_aqi_level"]
+        ), "o3 aqi value does not match!"
         assert (
                 document["so2_value"] == expected_values["so2_value"]
         ), "o2 value does not match!"
         assert (
+                document["so2_aqi_level"] == expected_values["so2_aqi_level"]
+        ), "so2 aqi value does not match!"
+        assert (
                 document["pm10_value"] == expected_values["pm10_value"]
         ), "pm10 value does not match!"
         assert (
+                document["pm10_aqi_level"] == expected_values["pm10_aqi_level"]
+        ), "pm10 aqi value does not match!"
+        assert (
                 document["pm2_5_value"] == expected_values["pm2_5_value"]
         ), "pm2.5 value does not match!"
+        assert (
+                document["pm2_5_aqi_level"] == expected_values["pm2_5_aqi_level"]
+        ), "pm2_5 aqi value does not match!"
 
 
 # aqi values
 
 
-def test_overall_aqi_level_is_between_1_and_6():
+def test__overall_aqi_level_is_between_1_and_6():
     dict_result = get_database_data({}, "forecast_data")
 
     for document in dict_result:
@@ -95,7 +163,7 @@ def test_overall_aqi_level_is_between_1_and_6():
         assert 1 <= overall_aqi_level <= 6, f" {overall_aqi_level} is out of range"
 
 
-def test_individual_aqi_levels_are_between_1_and_6():
+def test__individual_aqi_levels_are_between_1_and_6():
     dict_result = get_database_data({}, "forecast_data")
     pollutant_keys = [
         "no2_aqi_level",
@@ -109,7 +177,7 @@ def test_individual_aqi_levels_are_between_1_and_6():
             assert 1 <= document[key] <= 6, f"{key} {document[key]} is out of range"
 
 
-def test_overall_aqi_level_is_highest_value_of_pollutant_aqi_levels():
+def test__overall_aqi_level_is_highest_value_of_individual_pollutant_aqi_levels():
     dict_result = get_database_data({}, "forecast_data")
     pollutant_keys = [
         "no2_aqi_level",
@@ -127,8 +195,7 @@ def test_overall_aqi_level_is_highest_value_of_pollutant_aqi_levels():
 
 
 def test_that_each_document_has_location_type_city():
-    query = {}
-    dict_result = get_database_data(query, "forecast_data")
+    dict_result = get_database_data({}, "forecast_data")
 
     for document in dict_result:
         assert (
@@ -141,3 +208,13 @@ def test_document_count_for_single_day_is_6273():
 
     assert len(dict_result) == 6273, f"Expected 6273 documents, but got {len(dict_result)}"
 
+
+def test__created_time_is_similar_to_system_time():
+    dict_result = get_database_data({}, "forecast_data")
+    now = datetime.utcnow()
+    for document in dict_result:
+        created_time = document["created_time"]
+
+        assert now - timedelta(seconds=1000) <= created_time <= now, (
+
+        )
