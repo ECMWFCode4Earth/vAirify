b6d45767a6616eaf8f8a6ef57553ebe1a1a8d226
Richard Strange <rstrange@scottlogic.com>
2024-06-13 11:51:32 +0100

    55 parameterise base forecast time (#103)
    * Parameterised the initial forecast base date when fetching forecast data. Also updated to properly account for same day and previous day requests
    
    * Minor updates to the UTC times
    
    * Renamed environment override for forecast base time

--- air-quality-backend/scripts/run_forecast_etl.py
+++ air-quality-backend/scripts/run_forecast_etl.py
@@ -1,8 +1,9 @@
 import logging
+import os
 from logging import config
 
 from dotenv import load_dotenv
-
+from datetime import datetime
 from air_quality.database.forecasts import insert_data
 from air_quality.database.locations import get_locations_by_type, AirQualityLocationType
 from air_quality.etl.forecast.forecast_adapter import transform
@@ -17,8 +18,14 @@ def main():
     cities = get_locations_by_type(AirQualityLocationType.CITY)
     logging.info(f"Finding data for {cities.__len__()} cities")
 
+    base_date = datetime.utcnow()
+    base_date_env = os.environ.get("FORECAST_BASE_TIME")
+    if base_date_env is not None:
+        date_format = "%Y-%m-%d %H"
+        base_date = datetime.strptime(base_date_env, date_format)
+
     logging.info("Extracting pollutant forecast data")
-    extracted_forecast_data = fetch_forecast_data()
+    extracted_forecast_data = fetch_forecast_data(base_date)
 
     logging.info("Transforming forecast data")
     transformed_forecast_data = transform(extracted_forecast_data, cities)
