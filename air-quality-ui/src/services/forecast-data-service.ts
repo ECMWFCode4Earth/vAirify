67fc1bc2bacc633a8b3d5ead44ba6cfedd1d82c7
amehta <amehta@scottlogic.com>
2024-06-17 09:56:32 +0100

    Refactor api fetch code

--- air-quality-ui/src/services/forecast-data-service.ts
+++ air-quality-ui/src/services/forecast-data-service.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
 
+import { fetchJson } from './fetch-common'
 import { ForecastResponseDto } from './types'
 import { LocationType } from '../models'
 
@@ -17,15 +18,5 @@ export const getForecastData = async (
     valid_time_to: dateTo.toJSDate().toISOString(),
     base_time: baseTime.toJSDate().toISOString(),
   }
-  const url = new URL(`${API_URL}/air-pollutant/forecast`)
-  Object.keys(params).forEach((key) =>
-    url.searchParams.append(key, params[key]),
-  )
-
-  return fetch(url.toString(), {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json())
+  return fetchJson(`${API_URL}/air-pollutant/forecast`, params)
 }
