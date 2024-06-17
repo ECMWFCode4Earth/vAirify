2dcf7e147e33887a87832d24008dd76b20d1fbf4
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-06-17 16:11:45 +0100

    split 200 and 422 into separate tests

--- air-quality-backend/system_tests/api_suite/measurements_summary_api_validation_tests.py
+++ air-quality-backend/system_tests/api_suite/measurements_summary_api_validation_tests.py
@@ -14,83 +14,59 @@ measurement_base_time_string_24_7_20_14_0_0 = format_datetime_as_string(
 )
 measurement_time_range = 90
 
-success_status_code = 200
-validation_error_status_code = 422
+
+def test__required_parameters_provided__verify_response_status_200():
+
+    payload: dict = {
+        "location_type": location_type,
+        "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
+        "measurement_time_range": measurement_time_range,
+    }
+    response = requests.request("GET", base_url, params=payload, timeout=5.0)
+    assert response.status_code == 200
 
 
 @pytest.mark.parametrize(
-    "payload, expected_status_code",
+    "payload",
     [
-        (
-            {
-                "location_type": location_type,
-                "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
-                "measurement_time_range": measurement_time_range,
-            },
-            success_status_code,
-        ),
-        (
-            {
-                "location_type": "",
-                "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
-                "measurement_time_range": measurement_time_range,
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "location_type": location_type,
-                "measurement_base_time": "",
-                "measurement_time_range": measurement_time_range,
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "location_type": location_type,
-                "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
-                "measurement_time_range": "",
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "location_type": "",
-                "measurement_base_time": "",
-                "measurement_time_range": "",
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
-                "measurement_time_range": measurement_time_range,
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "location_type": location_type,
-                "measurement_time_range": measurement_time_range,
-            },
-            validation_error_status_code,
-        ),
-        (
-            {
-                "location_type": location_type,
-                "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
-            },
-            validation_error_status_code,
-        ),
-        (
-            {},
-            validation_error_status_code,
-        ),
+        {
+            "location_type": "",
+            "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
+            "measurement_time_range": measurement_time_range,
+        },
+        {
+            "location_type": location_type,
+            "measurement_base_time": "",
+            "measurement_time_range": measurement_time_range,
+        },
+        {
+            "location_type": location_type,
+            "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
+            "measurement_time_range": "",
+        },
+        {
+            "location_type": "",
+            "measurement_base_time": "",
+            "measurement_time_range": "",
+        },
+        {
+            "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
+            "measurement_time_range": measurement_time_range,
+        },
+        {
+            "location_type": location_type,
+            "measurement_time_range": measurement_time_range,
+        },
+        {
+            "location_type": location_type,
+            "measurement_base_time": measurement_base_time_string_24_7_20_14_0_0,
+        },
+        {},
     ],
 )
-def test__required_parameter_combinations__verify_response_status_code(
+def test__required_parameters_missing_or_empty__verify_response_status_422(
     payload: dict,
-    expected_status_code: int,
 ):
     response = requests.request("GET", base_url, params=payload, timeout=5.0)
-    assert response.status_code == expected_status_code
+
+    assert response.status_code == 422
