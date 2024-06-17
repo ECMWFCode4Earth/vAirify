888f6d1476c3d9b5db7978f599a00d597262f00d
amehta <amehta@scottlogic.com>
2024-06-17 09:56:32 +0100

    Refactor api fetch code

--- air-quality-ui/src/services/measurement-data-service.ts
+++ air-quality-ui/src/services/measurement-data-service.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
 
+import { fetchJson } from './fetch-common'
 import { MeasurementSummaryResponseDto, MeasurementsResponseDto } from './types'
 import { LocationType } from '../models'
 
@@ -9,21 +10,7 @@ const queryMeasurements = (
   urlString: string,
   params: Record<string, string | string[]>,
 ) => {
-  const url = new URL(`${API_URL}/air-pollutant${urlString}`)
-  Object.keys(params).forEach((key) => {
-    const value = params[key]
-    if (typeof value === 'string') {
-      url.searchParams.append(key, value)
-    } else {
-      value.forEach((v) => url.searchParams.append(key, v))
-    }
-  })
-  return fetch(url.toString(), {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json())
+  return fetchJson(`${API_URL}/air-pollutant${urlString}`, params)
 }
 
 export const getMeasurements = async (
