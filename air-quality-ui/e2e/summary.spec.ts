b6d8b7d6ff7e9859c53cd8bab5c7a186f21f6027
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-06-20 16:20:22 +0100

    breadcrumb mocked api test

--- air-quality-ui/e2e/summary.spec.ts
+++ air-quality-ui/e2e/summary.spec.ts
@@ -3,58 +3,12 @@ import { apiForecast, apiSummary } from "./mocked_api.ts";
 
 test("Mocked response breadcrumb", async ({ page }) => {
   await page.route("*/**/air-pollutant/forecast*", async (route) => {
-    const json = [
-      {
-        base_time: "2024-06-19T00:00:00Z",
-        valid_time: "2024-06-19T09:00:00Z",
-        location_type: "city",
-        location_name: "Kampala",
-        overall_aqi_level: 2,
-        no2: {
-          aqi_level: 1,
-          value: 0.3812829140487199,
-        },
-        o3: {
-          aqi_level: 2,
-          value: 72.9086035913633,
-        },
-        pm2_5: {
-          aqi_level: 2,
-          value: 16.067128848211063,
-        },
-        pm10: {
-          aqi_level: 2,
-          value: 26.087666551144732,
-        },
-        so2: {
-          aqi_level: 1,
-          value: 0.6314619719025142,
-        },
-      },
-    ];
-    await route.fulfill({ json });
+    await route.fulfill({ json: apiForecast });
   });
-
   await page.route(
     "*/**/air-pollutant/measurements/summary*",
     async (route) => {
-      const json = [
-        {
-          measurement_base_time: "2024-06-19T09:00:00Z",
-          location_type: "city",
-          location_name: "Kampala",
-          overall_aqi_level: {
-            mean: 6,
-          },
-          pm2_5: {
-            mean: {
-              aqi_level: 6,
-              value: 76,
-            },
-          },
-        },
-      ];
-      await route.fulfill({ json });
+      await route.fulfill({ json: apiSummary });
     }
   );
   await page.goto("http://localhost:5173/city/summary");
