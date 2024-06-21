010cf5484ca17966576e4e5603fa3ad9e45b111f
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-06-21 17:35:41 +0100

    first test paramatization

--- air-quality-backend/system_tests/api_suite/measurements_api_seeded_data_tests.py
+++ air-quality-backend/system_tests/api_suite/measurements_api_seeded_data_tests.py
@@ -124,9 +124,16 @@ date_from_string_24_6_21_14_0_0 = format_datetime_as_string(
     datetime.datetime(2024, 6, 21, 14, 0, 0, tzinfo=datetime.timezone.utc),
     "%Y-%m-%dT%H:%M:%SZ",
 )
+date_from_string_24_6_21_15_0_0 = format_datetime_as_string(
+    datetime.datetime(2024, 6, 21, 15, 0, 0),
+    "%Y-%m-%dT%H:%M:%SZ",
+)
 date_to_string_24_6_21_16_0_0 = format_datetime_as_string(
     datetime.datetime(2024, 6, 21, 16, 0, 0, tzinfo=datetime.timezone.utc),
     "%Y-%m-%dT%H:%M:%SZ",
+)
+date_to_string_24_7_29_15_0_0 = format_datetime_as_string(
+    datetime.datetime(2024, 7, 29, 15, 0, 0), "%Y-%m-%dT%H:%M:%SZ"
 )
 
 # Test Setup
@@ -139,6 +146,7 @@ seed_api_test_data(
         test_city_2_site_1_2024_6_21_13_59_0,
         test_city_2_site_1_2024_6_21_14_0_0,
         test_city_3_site_1_2024_6_21_14_30_0,
+        test_city_3_site_1_2024_6_21_14_45_0,
         test_city_3_site_2_2024_6_21_15_30_0,
         test_city_1_site_1_2024_6_21_15_0_0,
         test_city_2_site_2_2024_6_21_16_0_0,
@@ -157,27 +165,165 @@ seed_api_test_data(
                 "date_to": date_to_string_24_6_21_16_0_0,
                 "location_type": location_type,
             },
-            ["Test City 1", "Test City 2", "Test City 2", "Test City 3", "Test City 3"],
+            [
+                test_city_1_site_1_2024_6_21_15_0_0["name"],
+                test_city_2_site_1_2024_6_21_14_0_0["name"],
+                test_city_2_site_2_2024_6_21_16_0_0["name"],
+                test_city_3_site_1_2024_6_21_14_30_0["name"],
+                test_city_3_site_1_2024_6_21_14_45_0["name"],
+                test_city_3_site_2_2024_6_21_15_30_0["name"],
+            ],
         ),
-        # (
-        #     {
-        #         "date_from": date_from_string_24_6_12_14_0_0,
-        #         "date_to": date_to_string_24_6_12_16_0_0,
-        #         "location_type": location_type,
-        #     },
-        #     [],
-        # ),
+        (
+            {
+                "date_from": date_from_string_24_6_21_15_0_0,
+                "date_to": date_to_string_24_7_29_15_0_0,
+                "location_type": location_type,
+            },
+            [
+                test_city_1_site_1_2024_6_21_15_0_0["name"],
+                test_city_1_site_2_2024_6_21_16_1_0["name"],
+                test_city_2_site_2_2024_6_21_16_0_0["name"],
+                test_city_2_site_3_2024_7_28_9_0_0["name"],
+                test_city_3_site_2_2024_6_21_15_30_0["name"],
+            ],
+        ),
     ],
 )
-def test__different_measurement_dates__assert_location_name_filtered_appropriately(
+def test__different_measurement_dates__assert_response_location_names_are_correct(
     api_parameters: dict, expected_location_names: str
 ):
     load_dotenv(".env-qa")
     response = requests.request("GET", base_url, params=api_parameters, timeout=5.0)
 
-    actual_locations = get_list_of_key_values(response.json(), "location_name")
-    actual_locations.sort()
+    actual_location_names = get_list_of_key_values(response.json(), "location_name")
+    actual_location_names.sort()
 
-    print(actual_locations)
-    print(expected_location_names)
-    assert actual_locations == expected_location_names
+    assert actual_location_names == expected_location_names
+
+
+@pytest.mark.parametrize(
+    "api_parameters, expected_site_names",
+    [
+        (
+            {
+                "date_from": date_from_string_24_6_21_14_0_0,
+                "date_to": date_to_string_24_6_21_16_0_0,
+                "location_type": location_type,
+            },
+            [
+                test_city_1_site_1_2024_6_21_15_0_0["location_name"],
+                test_city_2_site_1_2024_6_21_14_0_0["location_name"],
+                test_city_2_site_2_2024_6_21_16_0_0["location_name"],
+                test_city_3_site_1_2024_6_21_14_30_0["location_name"],
+                test_city_3_site_1_2024_6_21_14_45_0["location_name"],
+                test_city_3_site_2_2024_6_21_15_30_0["location_name"],
+            ],
+        ),
+        (
+            {
+                "date_from": date_from_string_24_6_21_15_0_0,
+                "date_to": date_to_string_24_7_29_15_0_0,
+                "location_type": location_type,
+            },
+            [
+                test_city_1_site_1_2024_6_21_15_0_0["location_name"],
+                test_city_1_site_2_2024_6_21_16_1_0["location_name"],
+                test_city_2_site_2_2024_6_21_16_0_0["location_name"],
+                test_city_2_site_3_2024_7_28_9_0_0["location_name"],
+                test_city_3_site_2_2024_6_21_15_30_0["location_name"],
+            ],
+        ),
+    ],
+)
+def test__different_measurement_dates__assert_response_site_names_are_correct(
+    api_parameters: dict, expected_site_names: str
+):
+    load_dotenv(".env-qa")
+    response = requests.request("GET", base_url, params=api_parameters, timeout=5.0)
+
+    actual_site_names = get_list_of_key_values(response.json(), "site_name")
+    actual_site_names.sort()
+
+    assert actual_site_names == expected_site_names
+
+
+@pytest.mark.parametrize(
+    "api_parameters, expected_measurement_dates",
+    [
+        (
+            {
+                "date_from": date_from_string_24_6_21_14_0_0,
+                "date_to": date_to_string_24_6_21_16_0_0,
+                "location_type": location_type,
+            },
+            [
+                format_datetime_as_string(
+                    test_city_2_site_1_2024_6_21_14_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_3_site_1_2024_6_21_14_30_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_3_site_1_2024_6_21_14_45_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_1_site_1_2024_6_21_15_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_3_site_2_2024_6_21_15_30_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_2_site_2_2024_6_21_16_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+            ],
+        ),
+        (
+            {
+                "date_from": date_from_string_24_6_21_15_0_0,
+                "date_to": date_to_string_24_7_29_15_0_0,
+                "location_type": location_type,
+            },
+            [
+                format_datetime_as_string(
+                    test_city_1_site_1_2024_6_21_15_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_3_site_2_2024_6_21_15_30_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_2_site_2_2024_6_21_16_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_1_site_2_2024_6_21_16_1_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+                format_datetime_as_string(
+                    test_city_2_site_3_2024_7_28_9_0_0["measurement_date"],
+                    "%Y-%m-%dT%H:%M:%SZ",
+                ),
+            ],
+        ),
+    ],
+)
+def test__different_measurement_dates__assert_response_measurement_dates_are_correct(
+    api_parameters: dict, expected_measurement_dates: str
+):
+    load_dotenv(".env-qa")
+    response = requests.request("GET", base_url, params=api_parameters, timeout=5.0)
+
+    actual_measurement_dates = get_list_of_key_values(
+        response.json(), "measurement_date"
+    )
+    actual_measurement_dates.sort()
+
+    assert actual_measurement_dates == expected_measurement_dates
