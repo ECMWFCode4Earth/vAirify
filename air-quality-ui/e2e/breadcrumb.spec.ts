c6d47747a7a0022f7e352e9bb11e47e0796c404d
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-06-24 14:49:55 +0100

    typo fix

--- air-quality-ui/e2e/breadcrumb.spec.ts
+++ air-quality-ui/e2e/breadcrumb.spec.ts
@@ -25,5 +25,5 @@ test('Mocked response breadcrumb', async ({ page }) => {
   // Zurich Test
   await page.getByRole('link', { name: 'Zurich' }).click()
   await expect(page.locator('text=Cities')).toBeVisible()
-  await expect(page.locator('text=Zurici')).toBeVisible()
+  await expect(page.locator('text=Zurich')).toBeVisible()
 })
