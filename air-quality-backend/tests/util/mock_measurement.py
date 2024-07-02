7f3f080a35c36693461f170193414af75ff48146
rstrange-scottlogic <rstrange@scottlogic.com>
2024-07-02 15:20:52 +0100

    Updated the open AQ filter to ensure PM values for ppm are filtered out

--- air-quality-backend/tests/util/mock_measurement.py
+++ air-quality-backend/tests/util/mock_measurement.py
@@ -3,6 +3,32 @@ from datetime import datetime, timezone
 from air_quality.database.in_situ import InSituMeasurement, InSituAveragedMeasurement
 
 
+def create_mock_open_aq_response(overrides):
+    default_open_aq_mock = {
+        "locationId": 1,
+        "location": "Dublin measurement station",
+        "parameter": "so2",
+        "value": 10,
+        "date": {
+            "utc": "2024-04-21T00:00:00+00:00",
+            "local": "2024-04-21T01:00:00+01:00",
+        },
+        "unit": "µg/m³",
+        "coordinates": {
+            "latitude": 0.0,
+            "longitude": 0.0,
+        },
+        "country": "IE",
+        "city": None,
+        "isMobile": False,
+        "isAnalysis": None,
+        "entity": "Governmental Organization",
+        "sensorType": "reference grade",
+    }
+
+    return {**default_open_aq_mock, **overrides}
+
+
 def create_mock_measurement_document(overrides) -> InSituMeasurement:
     default_document = {
         "measurement_date": datetime(2024, 6, 4, 0, 0, tzinfo=timezone.utc),
