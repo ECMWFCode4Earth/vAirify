0bb7dfccd54d624f2f5ade2f6740154fd8d0a6f9
mwalker-scottlogic <110834691+mwalker-scottlogic@users.noreply.github.com>
2024-07-03 08:16:11 +0100

    Automation test for: 63 convert ppm measurements (#171)
    * initial test to set ppm
    
    * initial split into two tests
    
    * refactored into existing etl test suite
    
    * spelling
    
    * adding coordinates
    
    * changed pm pollutants to not use ppm
    
    * removing redundant mgm3 test
    
    * removing redundant mgm3 test
    
    * added test to check conversion
    
    * refactor
    
    * changing test to expect pm10 and pm2_5 filtered from data
    
    * linting

--- air-quality-backend/system_tests/in_situ_etl_suite/open_aq_etl_tests.py
+++ air-quality-backend/system_tests/in_situ_etl_suite/open_aq_etl_tests.py
@@ -14,9 +14,13 @@ from air_quality.database.in_situ import InSituMeasurement, InSituPollutantReadi
 from air_quality.database.locations import AirQualityLocationType
 from air_quality.etl.forecast.forecast_dao import fetch_forecast_data
 from system_tests.in_situ_etl_suite.open_aq_data_creator import (
-    create_open_aq_measurement)
+    create_open_aq_measurement,
+)
 from system_tests.utils.database_utilities import (
-    get_database_data, delete_database_data, seed_api_test_data)
+    get_database_data,
+    delete_database_data,
+    seed_api_test_data,
+)
 from scripts.run_in_situ_etl import main
 
 from dotenv import load_dotenv
@@ -42,10 +46,7 @@ def test__in_situ_etl__calling_actual_api_returns_values_and_stores():
 
 @mock.patch.dict(
     os.environ,
-    {
-        "OPEN_AQ_CITIES": "London",
-        "OPEN_AQ_CACHE": open_aq_cache_location
-    },
+    {"OPEN_AQ_CITIES": "London", "OPEN_AQ_CACHE": open_aq_cache_location},
 )
 @freeze_time("2024-05-25T13:00:00")
 def test__in_situ_etl__combines_pollutants_for_location_times():
@@ -81,10 +82,7 @@ def test__in_situ_etl__combines_pollutants_for_location_times():
 
 @mock.patch.dict(
     os.environ,
-    {
-        "OPEN_AQ_CITIES": "London",
-        "OPEN_AQ_CACHE": open_aq_cache_location
-    },
+    {"OPEN_AQ_CITIES": "London", "OPEN_AQ_CACHE": open_aq_cache_location},
 )
 @freeze_time("2024-05-25T13:00:00")
 def test__in_situ_etl__stores_all_data_correctly():
@@ -118,24 +116,25 @@ def test__in_situ_etl__stores_all_data_correctly():
     assert stored["location_type"] == "city"
     assert stored["api_source"] == "OpenAQ"
     assert stored["location"]["type"] == "point"
-    assert (stored["location"]["coordinates"][0]
-            == london_openaq_data[0]["coordinates"]["longitude"])
-    assert (stored["location"]["coordinates"][1]
-            == london_openaq_data[0]["coordinates"]["latitude"])
+    assert (
+        stored["location"]["coordinates"][0]
+        == london_openaq_data[0]["coordinates"]["longitude"]
+    )
+    assert (
+        stored["location"]["coordinates"][1]
+        == london_openaq_data[0]["coordinates"]["latitude"]
+    )
 
-    assert_pollutant_value(stored["no2"], 1)
-    assert_pollutant_value(stored["o3"], 2)
-    assert_pollutant_value(stored["so2"], 3)
-    assert_pollutant_value(stored["pm2_5"], 4)
-    assert_pollutant_value(stored["pm10"], 5)
+    assert_pollutant_value(stored["no2"], 1, "µg/m³", 1, "µg/m³")
+    assert_pollutant_value(stored["o3"], 2, "µg/m³", 2, "µg/m³")
+    assert_pollutant_value(stored["so2"], 3, "µg/m³", 3, "µg/m³")
+    assert_pollutant_value(stored["pm2_5"], 4, "µg/m³", 4, "µg/m³")
+    assert_pollutant_value(stored["pm10"], 5, "µg/m³", 5, "µg/m³")
 
 
 @mock.patch.dict(
     os.environ,
-    {
-        "OPEN_AQ_CITIES": "London,Melbourne",
-        "OPEN_AQ_CACHE": open_aq_cache_location
-    },
+    {"OPEN_AQ_CITIES": "London,Melbourne", "OPEN_AQ_CACHE": open_aq_cache_location},
 )
 @freeze_time("2024-05-25T13:00:00")
 def test__in_situ_etl__handles_multiple_cities():
@@ -167,10 +166,7 @@ def test__in_situ_etl__handles_multiple_cities():
 
 @mock.patch.dict(
     os.environ,
-    {
-        "OPEN_AQ_CITIES": "London",
-        "OPEN_AQ_CACHE": open_aq_cache_location
-    },
+    {"OPEN_AQ_CITIES": "London", "OPEN_AQ_CACHE": open_aq_cache_location},
 )
 @freeze_time("2024-05-25T13:00:00+0000")
 def test__in_situ_etl__updates_existing_data():
@@ -191,8 +187,8 @@ def test__in_situ_etl__updates_existing_data():
             "value": 17,
             "unit": "test",
             "original_value": 17,
-            "original_unit": "test"
-        }
+            "original_unit": "test",
+        },
     }
     seed_api_test_data(collection_name, [existing_measurement])
 
@@ -206,16 +202,14 @@ def test__in_situ_etl__updates_existing_data():
     results = get_database_data(collection_name, query)
     assert len(results) == 1
     assert results[0]["no2"]["value"] == 116
-    assert (results[0]["last_modified_time"]
-            == datetime(2024, 5, 25, 13, 0, tzinfo=timezone.utc))
+    assert results[0]["last_modified_time"] == datetime(
+        2024, 5, 25, 13, 0, tzinfo=timezone.utc
+    )
 
 
 @mock.patch.dict(
     os.environ,
-    {
-        "OPEN_AQ_CITIES": "London",
-        "OPEN_AQ_CACHE": open_aq_cache_location
-    },
+    {"OPEN_AQ_CITIES": "London", "OPEN_AQ_CACHE": open_aq_cache_location},
 )
 @freeze_time("2024-05-25T13:00:00")
 def test__in_situ_etl__invalid_data_raises_error_and_does_not_store():
@@ -251,9 +245,8 @@ def ensure_forecast_cache():
 @mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
 @mock.patch.dict(os.environ, {"OPEN_AQ_CITIES": "London"})
 def test__in_situ_etl__timeouts_retry_twice_then_stop(
-        mock_get_conn,
-        caplog,
-        ensure_forecast_cache):
+    mock_get_conn, caplog, ensure_forecast_cache
+):
     mock_get_conn.return_value.getresponse.return_value = mock_response_for_status(408)
     query = {"name": "London"}
     delete_database_data(collection_name, query)
@@ -271,9 +264,8 @@ def test__in_situ_etl__timeouts_retry_twice_then_stop(
 @mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
 @mock.patch.dict(os.environ, {"OPEN_AQ_CITIES": "London"})
 def test__in_situ_etl__internal_error_fails_without_retry(
-        mock_get_conn,
-        caplog,
-        ensure_forecast_cache):
+    mock_get_conn, caplog, ensure_forecast_cache
+):
     mock_get_conn.return_value.getresponse.return_value = mock_response_for_status(500)
     query = {"name": "London"}
     delete_database_data(collection_name, query)
@@ -291,13 +283,11 @@ def test__in_situ_etl__internal_error_fails_without_retry(
 @mock.patch("urllib3.connectionpool.HTTPConnectionPool._get_conn")
 @mock.patch.dict(os.environ, {"OPEN_AQ_CITIES": "London"})
 def test__in_situ_etl__timeout_followed_by_success_returns_correctly(
-        mock_get_conn,
-        caplog,
-        ensure_forecast_cache):
-
+    mock_get_conn, caplog, ensure_forecast_cache
+):
     mock_get_conn.return_value.getresponse.side_effect = [
         mock_response_for_status(408),
-        mock_response_for_status(200)
+        mock_response_for_status(200),
     ]
     query = {"name": "London"}
     delete_database_data(collection_name, query)
@@ -306,28 +296,145 @@ def test__in_situ_etl__timeout_followed_by_success_returns_correctly(
         main()
 
     assert "Response for London contained no results" not in caplog.text
-    assert ("URL was: https://api.openaq.org/v2/measurements?limit=3000" not in
-            caplog.text)
+    assert (
+        "URL was: https://api.openaq.org/v2/measurements?limit=3000" not in caplog.text
+    )
     results = get_database_data(collection_name, query)
     assert len(results) == 1
     assert results[0]["no2"]["value"] == 113
     assert len(mock_get_conn.return_value.request.mock_calls) == 2
 
 
+@mock.patch.dict(
+    os.environ,
+    {"OPEN_AQ_CITIES": "Berlin", "OPEN_AQ_CACHE": open_aq_cache_location},
+)
+@freeze_time("2024-06-27T13:00:00")
+def test__convert_ppm_to_ugm3_and_store__only_no2_so2_o3():
+
+    # delete Berlin data, set up test data in file for Berlin
+    query = {"name": "Berlin"}
+    delete_database_data(collection_name, query)
+    berlin_file = f"{open_aq_cache_location}/Berlin_2024062613_2024062713.json"
+
+    location_berlin = "berlin"
+    latitude_berlin = 52.52437
+    longitude_berlin = 13.41053
+    unit_ppm = "ppm"
+    date_utc = "2024-06-26T13:10:20+00:00"
+
+    berlin_openaq_data = [
+        create_measurement(
+            date_utc,
+            "no2",
+            1000,
+            location_berlin,
+            unit_ppm,
+            longitude_berlin,
+            latitude_berlin,
+        ),
+        create_measurement(
+            date_utc,
+            "o3",
+            1000,
+            location_berlin,
+            unit_ppm,
+            longitude_berlin,
+            latitude_berlin,
+        ),
+        create_measurement(
+            date_utc,
+            "so2",
+            1000,
+            location_berlin,
+            unit_ppm,
+            longitude_berlin,
+            latitude_berlin,
+        ),
+        create_measurement(
+            date_utc,
+            "pm25",
+            1000,
+            location_berlin,
+            unit_ppm,
+            longitude_berlin,
+            latitude_berlin,
+        ),
+        create_measurement(
+            date_utc,
+            "pm10",
+            1000,
+            location_berlin,
+            unit_ppm,
+            longitude_berlin,
+            latitude_berlin,
+        ),
+    ]
+
+    write_to_file(berlin_openaq_data, berlin_file)
+
+    # run in situ ETL process, use file as cached response from OpenAQ for Berlin
+    main()
+    os.remove(berlin_file)
+
+    # investigating data stored for Berlin
+    results = get_database_data(collection_name, query)
+    stored: InSituMeasurement = results[0]
+    estimated_surface_pressure_pa = stored["metadata"]["estimated_surface_pressure_pa"]
+    estimated_temperature_k = stored["metadata"]["estimated_temperature_k"]
+    estimated_surface_pressure_hpa = estimated_surface_pressure_pa / 100
+    molecular_volume = (
+        22.41
+        * (estimated_temperature_k / 273)
+        * (1013 / estimated_surface_pressure_hpa)
+    )
+    expected_no2_ugm3 = convert_ppm_to_ugm3(
+        stored["no2"]["original_value"], 46.01, molecular_volume
+    )
+    expected_o3_ugm3 = convert_ppm_to_ugm3(
+        stored["o3"]["original_value"], 48, molecular_volume
+    )
+    expected_so2_ugm3 = convert_ppm_to_ugm3(
+        stored["so2"]["original_value"], 64.07, molecular_volume
+    )
+
+    assert estimated_surface_pressure_pa == 100599.76759705569
+    assert estimated_temperature_k == 301.556091512509
+    assert_pollutant_value(stored["no2"], expected_no2_ugm3, "µg/m³", 1000, "ppm")
+    assert_pollutant_value(stored["o3"], expected_o3_ugm3, "µg/m³", 1000, "ppm")
+    assert_pollutant_value(stored["so2"], expected_so2_ugm3, "µg/m³", 1000, "ppm")
+    assert "pm10" not in stored
+    assert "pm2_5" not in stored
+
+
 def mock_response_for_status(status):
     def stream_response(chunk_size, decode_content):
         result = str(create_measurement("2024-05-24T13:10:20+00:00", "no2", 113))
         result = result.replace("'", '"')  # replace single with double quotes
-        return [bytes(f'{{"results": [{result}]}}', 'utf-8')]
+        return [bytes(f'{{"results": [{result}]}}', "utf-8")]
 
     return Mock(status=status, msg=HTTPMessage(), headers={}, stream=stream_response)
 
 
-def assert_pollutant_value(stored_data: InSituPollutantReading, expected_value: float):
+def convert_ppm_to_ugm3(
+    ppm_original_value,
+    molecular_weight_g_mol: float,
+    molecular_volume: float,
+):
+    return ppm_original_value * 1000 * (molecular_weight_g_mol / molecular_volume)
+
+
+def assert_pollutant_value(
+    stored_data: InSituPollutantReading,
+    expected_value: float,
+    expected_unit: str,
+    expected_original_value: float,
+    expected_original_unit: str,
+):
     assert stored_data["value"] == expected_value
-    assert stored_data["unit"] == "µg/m³"
-    assert stored_data["original_value"] == expected_value
-    assert stored_data["original_unit"] == "µg/m³"
+    assert stored_data["unit"] == expected_unit
+    assert stored_data["original_value"] == expected_original_value
+    assert stored_data["original_unit"] == expected_original_unit
 
 
 def retrieve_single(array, element_to_check, value_to_find):
@@ -337,16 +444,20 @@ def retrieve_single(array, element_to_check, value_to_find):
 
 
 def create_measurement(
-        date_str: str,
-        pollutant: str,
-        value: float,
-        location: str = "london"):
+    date_str: str,
+    pollutant: str,
+    value: float,
+    location: str = "london",
+    unit: str = "µg/m³",
+    longitude: float = 1111,
+    latitude: float = 1112,
+):
     overrides = {
         "location": f"{location}_test_station",
-        "date": {
-            "utc": date_str
-        },
+        "date": {"utc": date_str},
         "value": value,
-        "parameter": pollutant
+        "unit": unit,
+        "parameter": pollutant,
+        "coordinates": {"longitude": longitude, "latitude": latitude},
     }
     return create_open_aq_measurement(overrides)
