2236a3074a05346a75346e3baa6ad004e435076d
sebsteinig <seb.steinig@gmail.com>
2024-07-09 23:50:15 +0100

    refactoring to separate texture conversion and writing

--- air-quality-backend/etl/src/forecast/forecast_adapter.py
+++ air-quality-backend/etl/src/forecast/forecast_adapter.py
@@ -4,14 +4,12 @@ from typing import List, Dict, Tuple, Optional
 import logging
 from typing import TypedDict
 from .forecast_data import ForecastData, convert_to_forecast_data_type
+from .forecast_texture_storer import save_data_textures
 from shared.src.database.locations import AirQualityLocation
 from shared.src.aqi import calculator as aqi_calculator
 from shared.src.aqi.pollutant_type import PollutantType
-import os
 import xarray as xr
 import numpy as np
-from PIL import Image
-import pandas as pd
 
 
 class PollutantData(TypedDict):
@@ -217,119 +215,10 @@ def _convert_data(
     return rgb_data_array, min_val, max_val, units, num_lon, time
 
 
-def _chunk_data_array(
-    rgb_data_array: np.ndarray,
-    num_time_steps: int,
-    num_lon: int,
-    time_vector: xr.DataArray,
-) -> Tuple[List[np.ndarray], Dict[int, Dict[str, str]]]:
-    """
-    Split the rgb_data_array into smaller parts along the num_lon * num_time dimension
-    for smaller file sizes and faster loading times.
-
-    :param rgb_data_array: array with shape (num_lat, num_lon * num_time, channels)
-    :param num_time_steps: Number of time steps to use for each chunk
-    :param num_lon: Number of longitude points
-    :param time_vector: DataArray containing time information for each time step
-    :return: A list of chunked arrays and a dictionary with start and end time
-    stamps for each chunk
-    """
-    num_lat, total_lon_time, channels = rgb_data_array.shape
-    num_total_time_steps = total_lon_time // num_lon
-
-    chunks = []
-    time_steps_dict = {}
-
-    if num_time_steps <= 0 or num_time_steps > num_total_time_steps:
-        num_time_steps = num_total_time_steps
-
-    for i in range(0, num_total_time_steps, num_time_steps):
-        start_time_step = i
-        end_time_step = min(i + num_time_steps, num_total_time_steps)
-
-        start_index = start_time_step * num_lon
-        end_index = end_time_step * num_lon
-
-        chunk = rgb_data_array[:, start_index:end_index, :]
-        chunks.append(chunk)
-
-        time_start = pd.to_datetime(
-            time_vector.data[start_time_step:end_time_step].min(), unit="s"
-        ).strftime("%Y-%m-%d %H:%M:%S")
-        time_end = pd.to_datetime(
-            time_vector.data[start_time_step:end_time_step].max(), unit="s"
-        ).strftime("%Y-%m-%d %H:%M:%S")
-        time_steps_dict[len(chunks) - 1] = {
-            "time_start": time_start,
-            "time_end": time_end,
-        }
-
-    return chunks, time_steps_dict
-
-
-def _create_output_directory(forecast_data: ForecastData) -> Tuple[str, str]:
-    """
-    Create output directory for data textures.
-
-    :param forecast_data: The forecast data object.
-
-    :return: The output directory path and forecast date.
-    """
-
-    forecast_date = datetime.fromtimestamp(
-        forecast_data.get_time_value(), timezone.utc
-    ).strftime("%Y-%m-%d_%H")
-
-    output_directory = f"/app/data_textures/{forecast_date}"
-    if not os.path.exists(output_directory):
-        output_directory = f"{os.getcwd()}/data_textures/{forecast_date}"
-    os.makedirs(output_directory, exist_ok=True)
-    return output_directory, forecast_date
-
-
-def _save_data_texture(
-    rgb_data_array: np.ndarray,
-    output_directory: str,
-    forecast_date: str,
-    variable: str,
-    num_chunk: str,
-    total_chunks: str,
-    file_format: str,
-) -> str:
-    """
-    Save data textures to disk and convert to binary data.
-
-    :param rgb_data_array: Numpy array containing the image data.
-    :param output_directory: Directory to save the textures.
-    :param forecast_date: Forecast date string.
-    :param variable: Variable name to include in the filename.
-    :param num_chunk: Chunk number.
-    :param total_chunks: Total number of chunks.
-    :param file_format: File format for saving the texture.
-
-    :return: The path to the saved file.
-    """
-    if variable == "winds_10m":
-        image_array = rgb_data_array
-        format = "RGB"
-    else:
-        image_array = rgb_data_array.squeeze(axis=2)
-        format = "L"
-
-    image = Image.fromarray(image_array, format)
-    output_file = (
-        f"{output_directory}/{variable}_{forecast_date}_CAMS_global."
-        f"chunk_{num_chunk}_of_{total_chunks}.{file_format}"
-    )
-    image.save(output_file, format=file_format, lossless=True)
-    return output_file
-
-
 def _process_variable(
     data: xr.Dataset,
     data_type: str,
     variable_name: str,
-    output_directory: str,
     forecast_date: str,
 ) -> List[Dict[str, str]]:
     """
@@ -338,49 +227,26 @@ def _process_variable(
     :param data: The dataset containing the variable.
     :param data_type: The type of the data.
     :param variable_name: The name of the variable.
-    :param output_directory: Directory to save the textures.
     :param forecast_date: The forecast date.
 
-    :return: A list of dictionaries containing data texture metadata for database.
+    :return: A list of dictionaries containing data texture metadata for the database.
     """
-    documents = []
-    chunks_per_texture = 16
-    file_format = "webp"
 
     rgb_data_array, min_value, max_value, units, num_lon, time_vector = _convert_data(
         data, data_type
     )
-    chunk_list, chunk_dict = _chunk_data_array(
-        rgb_data_array, chunks_per_texture, num_lon, time_vector
+
+    documents = save_data_textures(
+        rgb_data_array,
+        num_lon,
+        time_vector,
+        variable_name,
+        forecast_date,
+        min_value,
+        max_value,
+        units,
     )
 
-    # WebP has better compression, but maximum pixel dimension is 16383 x 16383.
-    if (num_lon * chunks_per_texture) > 16383:
-        file_format = "png"
-
-    for num_chunk, chunk in enumerate(chunk_list):
-        output_file = _save_data_texture(
-            chunk,
-            output_directory,
-            forecast_date,
-            variable_name,
-            num_chunk + 1,
-            len(chunk_list),
-            file_format,
-        )
-        document = {
-            "forecast_base_time": forecast_date,
-            "variable": variable_name,
-            "source": "cams-production",
-            "min_value": min_value,
-            "max_value": max_value,
-            "units": units,
-            "texture_uri": output_file,
-            "time_start": chunk_dict[num_chunk]["time_start"],
-            "time_end": chunk_dict[num_chunk]["time_end"],
-            "chunk": f"{num_chunk+1} of {len(chunk_list)}",
-        }
-        documents.append(document)
     return documents
 
 
@@ -390,7 +256,9 @@ def create_data_textures(forecast_data: ForecastData):
     :param forecast_data:
     :return: List of dictionaries containing data texture metadata for database
     """
-    output_directory, forecast_date = _create_output_directory(forecast_data)
+    forecast_date = datetime.fromtimestamp(
+        forecast_data.get_time_value(), timezone.utc
+    ).strftime("%Y-%m-%d_%H")
     db_metadata = []
 
     for pollutant in PollutantType:
@@ -401,7 +269,6 @@ def create_data_textures(forecast_data: ForecastData):
             dataset,
             forecast_data_type.value,
             pollutant.value,
-            output_directory,
             forecast_date,
         )
         for document in documents:
@@ -412,7 +279,6 @@ def create_data_textures(forecast_data: ForecastData):
         forecast_data._single_level_data,
         "winds_10m",
         "winds_10m",
-        output_directory,
         forecast_date,
     )
     for document in documents:
---
+++ air-quality-backend/etl/tests/forecast/forecast_texture_storer_test.py
@@ -0,0 +1,180 @@
+from datetime import datetime, timezone
+
+import pytest
+import xarray as xr
+import numpy as np
+from unittest.mock import patch
+
+from etl.src.forecast.forecast_texture_storer import (
+    _chunk_data_array,
+    _create_output_directory,
+    _write_texture_to_disk,
+    save_data_textures,
+)
+from shared.tests.util.mock_forecast_data import default_time
+
+
+@pytest.mark.parametrize(
+    "num_time_steps, expected_num_chunks",
+    [
+        (4, 3),  # Standard case: 3 chunks
+        (11, 1),  # Edge case: Only 1 chunk, as num_time_steps equals total time steps
+        (1, 11),  # Edge case: Each time step is its own chunk
+        (0, 1),  # Edge case: num_time_steps is 0, should default to 1 chunk
+        (-1, 1),  # Edge case: num_time_steps is negative, should default to 1 chunk
+        (
+            20,
+            1,
+        ),  # Edge case: should default to 1 chunk
+    ],
+)
+def test__chunk_data_array(num_time_steps, expected_num_chunks):
+    rgb_data_array = np.random.rand(11, 121, 1).astype(np.uint8)
+    num_lon = 11
+    time_vector = xr.DataArray(np.linspace(0, 10, 11), dims="time")
+
+    chunks, time_steps_dict = _chunk_data_array(
+        rgb_data_array, num_time_steps, num_lon, time_vector
+    )
+
+    assert len(chunks) == expected_num_chunks
+    assert all(chunk.shape[0] == 11 for chunk in chunks)
+    assert all(chunk.shape[2] == 1 for chunk in chunks)
+    assert len(time_steps_dict) == expected_num_chunks
+
+    for i in range(expected_num_chunks):
+        assert "time_start" in time_steps_dict[i]
+        assert "time_end" in time_steps_dict[i]
+
+
+@pytest.fixture
+def mock_getcwd():
+    with patch("os.getcwd", return_value="/current/working/directory"):
+        yield
+
+
+@pytest.fixture
+def mock_makedirs():
+    with patch("os.makedirs") as mock:
+        yield mock
+
+
+@patch("os.makedirs")
+@patch("os.getcwd", return_value="/mock/current/directory")
+@patch("os.path.exists", side_effect=lambda path: path.startswith("/app/data_textures"))
+def test__create_output_directory(mock_exists, mock_getcwd, mock_makedirs):
+    forecast_date = "2024-07-09"
+
+    # Test case where the directory does not exist in /app/data_textures
+    mock_exists.side_effect = lambda path: False
+    expected_directory = f"/mock/current/directory/data_textures/{forecast_date}"
+    result = _create_output_directory(forecast_date)
+    mock_makedirs.assert_called_once_with(expected_directory, exist_ok=True)
+    assert result == expected_directory
+
+    # Reset mocks for the next test case
+    mock_makedirs.reset_mock()
+    mock_exists.side_effect = lambda path: path == f"/app/data_textures/{forecast_date}"
+
+    # Test case where the directory exists in /app/data_textures
+    expected_directory = f"/app/data_textures/{forecast_date}"
+    result = _create_output_directory(forecast_date)
+    mock_makedirs.assert_called_once_with(expected_directory, exist_ok=True)
+    assert result == expected_directory
+
+
+@patch("PIL.Image.Image.save")
+def test__save_data_texture(mock_save):
+    rgb_data_array = np.random.rand(11, 121, 1).astype(np.uint8)
+    output_directory = "test_output"
+    forecast_date = datetime.fromtimestamp(default_time, timezone.utc).strftime(
+        "%Y-%m-%d_%H"
+    )
+    variable = "pm10"
+    num_chunk = 1
+    total_chunks = 3
+    file_format = "png"
+
+    output_file = _write_texture_to_disk(
+        rgb_data_array,
+        output_directory,
+        forecast_date,
+        variable,
+        num_chunk,
+        total_chunks,
+        file_format,
+    )
+
+    assert output_file == (
+        f"{output_directory}/{variable}_{forecast_date}_CAMS_global."
+        f"chunk_{num_chunk}_of_{total_chunks}.{file_format}"
+    )
+    mock_save.assert_called_once()
+
+
+@patch("etl.src.forecast.forecast_texture_storer._chunk_data_array")
+@patch("etl.src.forecast.forecast_texture_storer._create_output_directory")
+@patch("etl.src.forecast.forecast_texture_storer._write_texture_to_disk")
+def test__save_data_textures(
+    mock_write_texture_to_disk, mock_create_output_directory, mock_chunk_data_array
+):
+    rgb_data_array = np.random.rand(11, 121, 1).astype(np.uint8)
+    num_lon = 11
+    time_vector = xr.DataArray(np.linspace(0, 10, 11), dims="time")
+    variable_name = "pm10"
+    forecast_date = "2024-07-09_00"
+    min_value = 0
+    max_value = 100.0
+    units = "kg m**-3 * 1e-9"
+
+    # Mocking _chunk_data_array
+    mock_chunk_data_array.return_value = (
+        [
+            np.random.rand(11, 44, 1).astype(np.uint8),
+            np.random.rand(11, 44, 1).astype(np.uint8),
+            np.random.rand(11, 33, 1).astype(np.uint8),
+        ],
+        {
+            0: {"time_start": "2024-07-09 00:00:00", "time_end": "2024-07-09 03:00:00"},
+            1: {"time_start": "2024-07-09 03:00:00", "time_end": "2024-07-09 06:00:00"},
+            2: {"time_start": "2024-07-09 06:00:00", "time_end": "2024-07-09 09:00:00"},
+        },
+    )
+
+    mock_create_output_directory.return_value = "/mock/output/directory"
+
+    mock_write_texture_to_disk.side_effect = (
+        lambda chunk, output_directory, forecast_date, variable_name, chunk_num,
+        total_chunks, file_format:
+        f"/mock/output/directory/{forecast_date}_{variable_name}_chunk{chunk_num}."
+        f"{file_format}"
+    )
+
+    documents = save_data_textures(
+        rgb_data_array,
+        num_lon,
+        time_vector,
+        variable_name,
+        forecast_date,
+        min_value,
+        max_value,
+        units,
+    )
+
+    assert len(documents) == 3
+    for i, doc in enumerate(documents):
+        assert doc["forecast_base_time"] == forecast_date
+        assert doc["variable"] == variable_name
+        assert doc["source"] == "cams-production"
+        assert doc["min_value"] == min_value
+        assert doc["max_value"] == max_value
+        assert doc["units"] == units
+        assert (
+            doc["texture_uri"]
+            == f"/mock/output/directory/{forecast_date}_{variable_name}_chunk{i+1}.webp"
+        )
+        assert (
+            doc["time_start"] == mock_chunk_data_array.return_value[1][i]["time_start"]
+        )
+        assert doc["time_end"] == mock_chunk_data_array.return_value[1][i]["time_end"]
+        assert doc["chunk"] == f"{i+1} of 3"
