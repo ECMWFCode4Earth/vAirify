cfc719c0007787c698c29406629756aca3ab4103
sebsteinig <seb.steinig@gmail.com>
2024-07-10 00:47:09 +0100

    fixed lint issues

--- air-quality-backend/shared/tests/xarray_utils_test.py
+++ air-quality-backend/shared/tests/xarray_utils_test.py
@@ -7,11 +7,14 @@ from shared.tests.util.mock_forecast_data import (
     gridded_data_single_level,
 )
 
+
 def test__get_dimension_by_attr():
     lat = _get_dimension_by_attr(gridded_data_single_level, "units", "degrees_north")
     lon = _get_dimension_by_attr(gridded_data_single_level, "units", "degrees_east")
     time = _get_dimension_by_attr(gridded_data_single_level, "standard_name", "time")
-    non_existent = _get_dimension_by_attr(gridded_data_single_level, "units", "non_existent_unit")
+    non_existent = _get_dimension_by_attr(
+        gridded_data_single_level, "units", "non_existent_unit"
+    )
 
     assert lat is not None
     assert lon is not None
@@ -20,6 +23,7 @@ def test__get_dimension_by_attr():
     assert lon.attrs["units"] == "degrees_east"
     assert time.attrs["standard_name"] == "time"
     assert non_existent is None
+
 
 def test__get_dim_names():
     lat, lon, time = get_dim_names(gridded_data_single_level)
