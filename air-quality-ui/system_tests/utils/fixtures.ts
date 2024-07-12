59a11cb2cd8360a3f3a6422ee59e244e14148e01
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-07-12 11:42:03 +0100

    Revert "adding banner page and initial test"
    This reverts commit d491de53ce4cdcbd29d4a68449d3c1e6d73dbfe1.

--- air-quality-ui/system_tests/utils/fixtures.ts
+++ air-quality-ui/system_tests/utils/fixtures.ts
@@ -1,18 +1,13 @@
 import { test as base } from '@playwright/test'
 
-import { Banner } from '../pages/banner'
 import { VairifyCityPage } from '../pages/vAirify_city_page'
 import { VairifySummaryPage } from '../pages/vAirify_summary_page'
 
 type Fixtures = {
-  banner: Banner
   vairifySummaryPage: VairifySummaryPage
   vairifyCityPage: VairifyCityPage
 }
 const test = base.extend<Fixtures>({
-  banner: async ({ page }, use) => {
-    await use(new Banner(page))
-  },
   vairifySummaryPage: async ({ page }, use) => {
     await use(new VairifySummaryPage(page))
   },
