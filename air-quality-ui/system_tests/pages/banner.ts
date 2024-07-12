92a185a4a6a2d94a7b7106f05a34ee69d1652f6d
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-07-12 16:33:25 +0100

    added base page and extended

--- air-quality-ui/system_tests/pages/banner.ts
+++ air-quality-ui/system_tests/pages/banner.ts
@@ -1,10 +1,13 @@
 import { Locator, type Page } from '@playwright/test'
 
-export class Banner {
+import { BasePage } from './base_page'
+
+export class Banner extends BasePage {
   readonly page: Page
   readonly logo: Locator
 
   constructor(page: Page) {
+    super(page)
     this.page = page
     this.logo = page.getByAltText('vAirify')
   }
