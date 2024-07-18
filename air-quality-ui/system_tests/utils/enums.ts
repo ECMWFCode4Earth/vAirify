5627ab73f1a500ed6c28d7f48a23e1fec3683e6d
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-07-18 09:13:32 +0100

    renamed enums and added some

---
+++ air-quality-ui/system_tests/utils/enums.ts
@@ -0,0 +1,64 @@
+export enum Colours {
+  aqi1 = 'rgb(80, 240, 229)',
+  aqi2 = 'rgb(80, 204, 170)',
+  aqi3 = 'rgb(240, 230, 65)',
+  aqi4 = 'rgba(255, 80, 80, 0.5)',
+  aqi5 = 'rgb(150, 0, 50)',
+  aqi6 = 'rgb(125, 33, 129)',
+  aqiNa = 'rgb(235, 235, 235)',
+  notColoured = 'rgba(0, 0, 0, 0)',
+}
+
+export enum CaseAQI1 {
+  aqiLevel = 1,
+  no2 = 2,
+  o3 = 3,
+  pm2_5 = 4,
+  pm10 = 5,
+  so2 = 6,
+}
+
+export enum CaseAQI2 {
+  aqiLevel = 2,
+  no2 = 45,
+  o3 = 55,
+  pm2_5 = 15,
+  pm10 = 25,
+  so2 = 101,
+}
+
+export enum CaseAQI3 {
+  aqiLevel = 3,
+  no2 = 100.5,
+  o3 = 100,
+  pm2_5 = 22.5,
+  pm10 = 45.5,
+  so2 = 300.5,
+}
+
+export enum CaseAQI4 {
+  aqiLevel = 4,
+  no2 = 125,
+  o3 = 135,
+  pm2_5 = 26,
+  pm10 = 99,
+  so2 = 205,
+}
+
+export enum CaseAQI5 {
+  aqiLevel = 5,
+  no2 = 240,
+  o3 = 370,
+  pm2_5 = 60,
+  pm10 = 120,
+  so2 = 510,
+}
+
+export enum CaseAQI6 {
+  aqiLevel = 6,
+  no2 = 900,
+  o3 = 400,
+  pm2_5 = 80,
+  pm10 = 160,
+  so2 = 800,
+}
