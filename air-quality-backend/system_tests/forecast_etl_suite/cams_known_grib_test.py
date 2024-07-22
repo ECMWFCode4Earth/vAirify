d10b560a063bdbabc327470903609f90b1d12c86
rstrange-scottlogic <rstrange@scottlogic.com>
2024-07-22 15:38:46 +0100

    Reworked some of the forecast ETL system tests

--- air-quality-backend/system_tests/forecast_etl_suite/cams_known_grib_test.py
+++ air-quality-backend/system_tests/forecast_etl_suite/cams_known_grib_test.py
@@ -12,90 +12,67 @@ from system_tests.utils.database_utilities import (
 )
 
 load_dotenv()
-
-test_cases = [
-    # Close to -180 Longitude
-    (
-        {
-            "name": "Vancouver",
-            "forecast_valid_time": datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "location_type": "city",
-            "source": "cams-production",
-        },
-        {
-            "name": "Vancouver",
-            "forecast_valid_time": datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "no2": {"value": 10.115771485462055, "aqi_level": 1},
-            "o3": {"value": 63.39522524659603, "aqi_level": 2},
-            "so2": {"value": 2.000387638160907, "aqi_level": 1},
-            "pm10": {"value": 5.810208243195257, "aqi_level": 1},
-            "pm2_5": {"value": 3.6327574082026914, "aqi_level": 1},
-        },
-    ),
-    # Extreme Longitude Check 180
-    (
-        {
-            "name": "Wellington",
-            "forecast_valid_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "location_type": "city",
-            "source": "cams-production",
-        },
-        {
-            "name": "Wellington",
-            "forecast_valid_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "no2": {"value": 0.4782831135385457, "aqi_level": 1},
-            "o3": {"value": 56.25565199915405, "aqi_level": 2},
-            "so2": {"value": 0.3777866472717213, "aqi_level": 1},
-            "pm10": {"value": 16.71105682154486, "aqi_level": 1},
-            "pm2_5": {"value": 9.073147098209494, "aqi_level": 1},
-        },
-    ),
-    # Close to 0 longitude
-    (
-        {
-            "name": "London",
-            "forecast_valid_time": datetime(2024, 6, 4, 6, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "location_type": "city",
-            "source": "cams-production",
-        },
-        {
-            "name": "London",
-            "forecast_valid_time": datetime(2024, 6, 4, 6, 0, 0, tzinfo=timezone.utc),
-            "forecast_base_time": datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
-            "no2": {"value": 19.136068918675015, "aqi_level": 1},
-            "o3": {"value": 9.499161050717573, "aqi_level": 1},
-            "so2": {"value": 3.8678773476028074, "aqi_level": 1},
-            "pm10": {"value": 24.70699837214256, "aqi_level": 2},
-            "pm2_5": {"value": 17.1565041916872, "aqi_level": 2},
-        },
-    ),
-]
+forecast_base_time = datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc)
+data_query = {"forecast_base_time": {"$eq": forecast_base_time}}
 
 
 @pytest.fixture(scope="module")
 def setup_data():
     # Set up code
     with mock.patch.dict(os.environ, {"FORECAST_BASE_TIME": "2024-6-4 00"}):
-        delete_database_data("forecast_data")
+        delete_database_data("forecast_data", data_query)
         main()
         yield
 
 
-@pytest.mark.parametrize("query_params, expected_values", test_cases)
-def test__extreme_longitudes__to_assert_exact_values(
-    query_params, expected_values, setup_data
+@pytest.mark.parametrize(
+    "city_name, forecast_valid_time, expected_values",
+    [
+        # Close to -180 Longitude
+        (
+            "Vancouver",
+            datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+            {
+                "no2": {"value": 10.115771485462055, "aqi_level": 1},
+                "o3": {"value": 63.39522524659603, "aqi_level": 2},
+                "so2": {"value": 2.000387638160907, "aqi_level": 1},
+                "pm10": {"value": 5.810208243195257, "aqi_level": 1},
+                "pm2_5": {"value": 3.6327574082026914, "aqi_level": 1},
+            },
+        ),
+        # Extreme Longitude Check 180
+        (
+            "Wellington",
+            datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc),
+            {
+                "no2": {"value": 0.4782831135385457, "aqi_level": 1},
+                "o3": {"value": 56.25565199915405, "aqi_level": 2},
+                "so2": {"value": 0.3777866472717213, "aqi_level": 1},
+                "pm10": {"value": 16.71105682154486, "aqi_level": 1},
+                "pm2_5": {"value": 9.073147098209494, "aqi_level": 1},
+            },
+        ),
+        # Close to 0 longitude
+        (
+            "London",
+            datetime(2024, 6, 4, 6, 0, 0, tzinfo=timezone.utc),
+            {
+                "no2": {"value": 19.136068918675015, "aqi_level": 1},
+                "o3": {"value": 9.499161050717573, "aqi_level": 1},
+                "so2": {"value": 3.8678773476028074, "aqi_level": 1},
+                "pm10": {"value": 24.70699837214256, "aqi_level": 2},
+                "pm2_5": {"value": 17.1565041916872, "aqi_level": 2},
+            },
+        ),
+    ]
+)
+def test__cities_at_extreme_longitudes__have_correct_forecast_values(
+        city_name, forecast_valid_time, expected_values, setup_data
 ):
     query = {
-        "name": query_params["name"],
-        "forecast_valid_time": {"$eq": query_params["forecast_valid_time"]},
-        "forecast_base_time": {"$eq": query_params["forecast_base_time"]},
-        "location_type": query_params["location_type"],
-        "source": query_params["source"],
+        "name": city_name,
+        "forecast_valid_time": {"$eq": forecast_valid_time},
+        "forecast_base_time": {"$eq": forecast_base_time},
     }
     stored_results = get_database_data("forecast_data", query)
     assert len(stored_results) == 1
@@ -104,11 +81,8 @@ def test__extreme_longitudes__to_assert_exact_values(
     _assert_dictionary_subset(expected_values, stored_results[0])
 
 
-# aqi values
-
-
 def test__overall_aqi_level_is_between_1_and_6(setup_data):
-    dict_result = get_database_data("forecast_data")
+    dict_result = get_database_data("forecast_data", data_query)
 
     for document in dict_result:
         overall_aqi_level = document["overall_aqi_level"]
@@ -116,47 +90,50 @@ def test__overall_aqi_level_is_between_1_and_6(setup_data):
 
 
 def test__individual_aqi_levels_are_between_1_and_6(setup_data):
-    dict_result = get_database_data("forecast_data")
+    dict_result = get_database_data("forecast_data", data_query)
     pollutant_keys = ["no2", "so2", "o3", "pm10", "pm2_5"]
     for document in dict_result:
         for key in pollutant_keys:
             assert (
-                1 <= document[key]["aqi_level"] <= 6
+                    1 <= document[key]["aqi_level"] <= 6
             ), f"{key} {document[key]['aqi_level']} is out of range"
 
 
 def test__overall_aqi_level_is_highest_value_of_individual_pollutant_aqi_levels(
-    setup_data,
+        setup_data,
 ):
-    dict_result = get_database_data("forecast_data")
+    dict_result = get_database_data("forecast_data", data_query)
     pollutant_keys = ["no2", "so2", "o3", "pm10", "pm2_5"]
 
     for document in dict_result:
         highest_aqi = max(document[key]["aqi_level"] for key in pollutant_keys)
         assert (
-            document["overall_aqi_level"] == highest_aqi
+                document["overall_aqi_level"] == highest_aqi
         ), f"{document['overall_aqi_level']} is not equal to {highest_aqi}"
 
 
 def test__that_each_document_has_location_type_city(setup_data):
-    dict_result = get_database_data("forecast_data")
+    dict_result = get_database_data("forecast_data", data_query)
 
     for document in dict_result:
         assert (
-            document["location_type"] == "city"
+                document["location_type"] == "city"
         ), f"location_type '{document['location_type']}' is not a city!"
 
 
-def test__document_count_for_single_day_is_6273(setup_data):
-    dict_result = get_database_data("forecast_data")
+def test__document_count_for_single_day_matches_expected(setup_data):
+    dict_result = get_database_data("forecast_data", data_query)
+
+    # no of locations * expected doc count
+    expected_doc_count = 153 * 41
 
     assert (
-        len(dict_result) == 6273
-    ), f"Expected 6273 documents, but got {len(dict_result)}"
+            len(dict_result) == expected_doc_count
+    ), f"Expected {expected_doc_count} documents, but got {len(dict_result)}"
 
 
 def test__created_time_exists(setup_data):
-    dict_result = get_database_data("forecast_data")
+    dict_result = get_database_data("forecast_data", data_query)
     for document in dict_result:
         assert "created_time" in document, "A document is missing the created_time key!"
 
