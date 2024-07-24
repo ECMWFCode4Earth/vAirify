d8c85a5734439fd3adddec31ed0774c1d790805f
sebsteinig <seb.steinig@gmail.com>
2024-07-24 20:24:49 +0100

    fixed UI linting issues

--- air-quality-ui/vite.config.ts
+++ air-quality-ui/vite.config.ts
@@ -1,10 +1,14 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import react from '@vitejs/plugin-react'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'data_textures'),
+  publicDir: path.resolve(
+    path.dirname(fileURLToPath(import.meta.url)),
+    'data_textures'
+  ),
 })
