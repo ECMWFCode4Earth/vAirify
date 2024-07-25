0463286c68003baa9726692c432235310140c355
rstrange-scottlogic <rstrange@scottlogic.com>
2024-07-25 16:30:33 +0100

    Moved time conversion from inner loop to speed up performance

--- air-quality-ui/src/services/calculate-measurements-aqi-averages/calculate-measurement-aqi-averages-service.ts
+++ air-quality-ui/src/services/calculate-measurements-aqi-averages/calculate-measurement-aqi-averages-service.ts
@@ -42,11 +42,12 @@ export const sortMeasurements = (
   const sortedMeasurements: SortMeasurementsType =
     generatePreAveragedDataStructure(baseTime)
   for (let i = 0; i < measurementsData.length; i++) {
+    const measurementDataTime = DateTime.fromISO(
+      measurementsData[i].measurement_date,
+    )
+
     for (const timeOffset in sortedMeasurements) {
       const bucket = sortedMeasurements[timeOffset]
-      const measurementDataTime = DateTime.fromISO(
-        measurementsData[i].measurement_date,
-      )
       if (
         bucket.lowerBound < measurementDataTime &&
         bucket.upperBound > measurementDataTime
