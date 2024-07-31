90b3052e563f074c07f1020c971472f41694804e
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-07-31 13:12:54 +0100

    refactor to separate tests out

--- air-quality-ui/system_tests/ui/banner.spec.ts
+++ air-quality-ui/system_tests/ui/banner.spec.ts
@@ -1,9 +1,6 @@
 import { expect, test } from '../utils/fixtures'
 import { gotoPage, setupPageWithMockData } from '../utils/helper_methods'
-import {
-  createForecastAPIResponseData,
-  createMeasurementSummaryAPIResponseData,
-} from '../utils/mocked_api'
+import { createForecastAPIResponseData } from '../utils/mocked_api'
 ;[
   {
     url: '/city/Rio%20de%20Janeiro',
@@ -14,58 +11,85 @@ import {
     pageType: 'summary',
   },
 ].forEach(({ url, pageType }) => {
-  test(`vAirify logo is visible on ${pageType} page`, async ({
-    page,
-    banner,
-  }) => {
-    await gotoPage(page, url)
-    await expect(banner.logo).toBeVisible()
-  }),
-    test(`Verify times can only be 12:00 or 00:00 on ${pageType} page`, async ({
-      page,
+  test.describe('Basic checks', () => {
+    test.beforeEach(async ({ page }) => {
+      await gotoPage(page, url)
+    }),
+      test(`vAirify logo is visible on ${pageType} page`, async ({
+        banner,
+      }) => {
+        await expect(banner.logo).toBeVisible()
+      }),
+      test(`Date picker is visible on ${pageType} page`, async ({ banner }) => {
+        await expect(banner.datePicker).toBeVisible()
+      })
+    test(`Verify date picker times can only be 12:00 or 00:00 on ${pageType} page`, async ({
       banner,
     }) => {
-      await gotoPage(page, url)
       await banner.calendarIcon.click()
 
       await expect(banner.datePickerTimeOptions).toHaveCount(2)
       await expect(banner.datePickerTimeOption0000).toBeVisible()
       await expect(banner.datePickerTimeOption1200).toBeVisible()
-    }),
-    test.beforeEach(async ({ page }) => {
-      const mockSystemDate: Date = new Date('2024-07-26T10:00:00Z')
-      await page.clock.setFixedTime(mockSystemDate)
-
-      await gotoPage(page, url)
-    }),
-    test(`Date picker is visible and cannot select a future day ${pageType} page`, async ({
-      banner,
-    }) => {
-      await expect(banner.datePicker).toBeVisible()
-
-      await banner.calendarIcon.click()
-      await expect(banner.futureDay27).toBeDisabled()
-      await expect(banner.datePickerNextMonthButton).toBeDisabled()
-
-      await banner.datePickerYearOpenButton.click()
-      await expect(banner.year2025).toBeDisabled()
-    }),
-    test(`Date picker is visible and cannot select a future time on ${pageType} page`, async ({
-      banner,
-    }) => {
-      await banner.calendarIcon.click()
-      await banner.clickOnDay(26)
-
-      await expect(banner.datePickerTimeOptions).toHaveCount(1)
-      await expect(banner.datePickerTimeOption1200).not.toBeVisible()
     })
+  })
+})
+;[
+  {
+    url: '/city/Rio%20de%20Janeiro',
+    pageType: 'city',
+  },
+  {
+    url: '/city/summary',
+    pageType: 'summary',
+  },
+].forEach(({ url, pageType }) => {
+  test.describe('Future dates and times are not possible', () => {
+    test.beforeEach(async ({ page, banner }) => {
+      const mockSystemDate: Date = new Date('2024-07-26T10:00:00Z')
+
+      await page.clock.setFixedTime(mockSystemDate)
+      await gotoPage(page, url)
+      await banner.calendarIcon.click()
+    }),
+      test(`Date picker cannot select a future day on ${pageType} page`, async ({
+        banner,
+      }) => {
+        await expect(banner.futureDay27).toBeDisabled()
+
+        await banner.datePickerYearOpenButton.click()
+        await expect(banner.year2025).toBeDisabled()
+      }),
+      test(`Date picker cannot select a future month on ${pageType} page`, async ({
+        banner,
+      }) => {
+        await expect(banner.datePickerNextMonthButton).toBeDisabled()
+
+        await banner.datePickerYearOpenButton.click()
+        await expect(banner.year2025).toBeDisabled()
+      }),
+      test(`Date picker cannot select a future year on ${pageType} page`, async ({
+        banner,
+      }) => {
+        await banner.datePickerYearOpenButton.click()
+        await expect(banner.year2025).toBeDisabled()
+      }),
+      test(`Date picker cannot select a future time on ${pageType} page`, async ({
+        banner,
+      }) => {
+        await banner.clickOnDay(26)
+
+        await expect(banner.datePickerTimeOptions).toHaveCount(1)
+        await expect(banner.datePickerTimeOption1200).not.toBeVisible()
+      })
+  })
 })
 
 test.describe('Range label', () => {
   test.beforeEach(async ({ page }) => {
     const mockSystemDate: Date = new Date('2024-07-26T10:00:00Z')
-    await page.clock.setFixedTime(mockSystemDate)
 
+    await page.clock.setFixedTime(mockSystemDate)
     await gotoPage(page, '/city/summary')
   })
 
@@ -101,60 +125,16 @@ test('Verify breadcrumb text is correct on each page', async ({
       location_type: 'city',
       location_name: 'Kampala',
     }),
-    createForecastAPIResponseData({
-      base_time: '2024-06-19T00:00:00Z',
-      valid_time: '2024-06-19T12:00:00Z',
-      location_type: 'city',
-      location_name: 'Abu Dhabi',
-    }),
-    createForecastAPIResponseData({
-      base_time: '2024-06-19T00:00:00Z',
-      valid_time: '2024-06-19T12:00:00Z',
-      location_type: 'city',
-      location_name: 'Zurich',
-    }),
-    createForecastAPIResponseData({
-      base_time: '2024-06-24T00:00:00Z',
-      valid_time: '2024-06-24T09:00:00Z',
-      location_type: 'city',
-      location_name: 'Kyiv',
-    }),
   ]
 
-  const mockedMeasurementSummaryResponse = [
-    createMeasurementSummaryAPIResponseData({
-      measurement_base_time: '2024-06-19T09:00:00Z',
-      location_name: 'Kampala',
-    }),
-    createMeasurementSummaryAPIResponseData({
-      measurement_base_time: '2024-06-19T12:00:00Z',
-      location_name: 'Abu Dhabi',
-    }),
-    createMeasurementSummaryAPIResponseData({
-      measurement_base_time: '2024-06-19T12:00:00Z',
-      location_name: 'Zurich',
-    }),
-  ]
   await setupPageWithMockData(page, [
     {
       endpointUrl: '*/**/air-pollutant/forecast*',
       mockedAPIResponse: mockedForecastResponse,
     },
-    {
-      endpointUrl: '*/**/air-pollutant/measurements/summary*',
-      mockedAPIResponse: mockedMeasurementSummaryResponse,
-    },
   ])
   await gotoPage(page, '/city/summary')
   await summaryPage.clickLinkByText('Kampala')
   await expect(cityPage.toolbarTextFinder('Cities/Kampala')).toBeVisible()
-  await summaryPage.clickLinkByText('Cities')
-
-  await summaryPage.clickLinkByText('Abu Dhabi')
-  await expect(cityPage.toolbarTextFinder('Cities/Abu Dhabi')).toBeVisible()
-  await summaryPage.clickLinkByText('Cities')
-
-  await summaryPage.clickLinkByText('Zurich')
-  await expect(cityPage.toolbarTextFinder('Cities/Zurich')).toBeVisible()
   await summaryPage.clickLinkByText('Cities')
 })
