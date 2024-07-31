2ea985dfcf6081a514105931634cda8470b0b241
Luke Vincent <11511477+lincent@users.noreply.github.com>
2024-07-31 17:00:14 +0100

    add new map config tests

--- air-quality-ui/src/components/single-city/station-map/map-service.spec.ts
+++ air-quality-ui/src/components/single-city/station-map/map-service.spec.ts
@@ -1,3 +1,4 @@
+import { MapOptions, RasterSourceSpecification, StyleSpecification } from 'maplibre-gl'
 import { createMapConfig } from './map-service'
 
 describe('map config', () => {
@@ -18,4 +19,32 @@ describe('map config', () => {
     const mapConfig = createMapConfig('id', 1, 2, 3)
     expect(mapConfig.container).toBe('id')
   })
+
+  it('is correctly attributed', () => {
+    const mapConfig = createMapConfig('id', 1, 2, 3)
+    expect(getRasterSource(mapConfig).attribution).toContain(
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    )
+  })
+
+  it('is uses correct tiles', () => {
+    const mapConfig = createMapConfig('id', 1, 2, 3)
+    expect(getRasterSource(mapConfig).tiles).toContain(
+      'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    )
+  })
+
+  it('has osm layer set', () => {
+    const mapConfig = createMapConfig('id', 1, 2, 3)
+    expect(
+      (mapConfig.style as StyleSpecification).layers.find(
+        (layer) => layer.id == 'osm',
+      ),
+    ).toBeDefined()
+  })
 })
+
+const getRasterSource = (mapConfig: MapOptions): RasterSourceSpecification => {
+  const sources = (mapConfig.style as StyleSpecification).sources
+  return sources.osm as RasterSourceSpecification
+}
