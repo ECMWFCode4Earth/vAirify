b6c737e540da3cf7810fc9d3940022e6f0bee5a0
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-07-31 15:45:32 +0100

    New snapshotsa and extreme boundary mocks

--- air-quality-ui/system_tests/pages/banner.ts
+++ air-quality-ui/system_tests/pages/banner.ts
@@ -6,11 +6,17 @@ export class Banner extends BasePage {
   readonly page: Page
 
   readonly logo: Locator
+  readonly dateOkButton: Locator
 
   constructor(page: Page) {
     super(page)
     this.page = page
 
     this.logo = page.getByAltText('vAirify')
+    this.dateOkButton = page.getByRole('button', { name: 'Ok' })
+  }
+
+  async confirmDate() {
+    await this.dateOkButton.click()
   }
 }
--- air-quality-ui/system_tests/ui/city_insitu.spec.ts
+++ air-quality-ui/system_tests/ui/city_insitu.spec.ts
@@ -12,132 +12,165 @@ import {
 test.use({
   viewport: { width: 1920, height: 1080 },
 })
-test('PM2.5 creates AQI 3 at 00:00', async ({ page, cityPage }) => {
-  const mockedForecastResponse = [
-    createForecastAPIResponseData({
-      base_time: '2024-07-01T00:00:00Z',
-      valid_time: '2024-07-01T00:00:00Z',
-      location_name: 'Rio de Janeiro',
-      overall_aqi_level: 1,
-    }),
-    createForecastAPIResponseData({
-      base_time: '2024-07-01T00:00:00Z',
-      valid_time: '2024-07-01T03:00:00Z',
-      location_name: 'Rio de Janeiro',
-      overall_aqi_level: 2,
-    }),
-    createForecastAPIResponseData({
-      base_time: '2024-07-01T00:00:00Z',
-      valid_time: '2024-07-01T06:00:00Z',
-      location_name: 'Rio de Janeiro',
-      overall_aqi_level: 3,
-    }),
-  ]
 
-  const mockedMeasurementsCityPageResponse = [
-    //AQI 1 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T00:00:00Z',
-      pm2_5: 10,
-      pm10: 20,
-      so2: 100,
-      no2: 40,
-      o3: 50,
-      site_name: 'Tijuca',
-    }),
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T00:00:00Z',
-      pm2_5: 20,
-      pm10: 40,
-      so2: 200,
-      no2: 90,
-      o3: 100,
-      site_name: 'AQI 2 Site',
-    }),
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T00:00:00Z',
-      pm2_5: 25,
-      pm10: 50,
-      so2: 350,
-      no2: 120,
-      o3: 130,
-      site_name: 'AQI 3 site',
-    }),
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T00:00:00Z',
-      pm2_5: 50,
-      pm10: 100,
-      so2: 500,
-      no2: 230,
-      o3: 240,
-      site_name: 'AQI 4 site',
-    }),
+test.describe('AQI is 3 at 00:00', () => {
+  test.beforeEach(async ({ page, banner, cityPage }) => {
+    const mockedForecastResponse = [
+      createForecastAPIResponseData({
+        base_time: '2024-07-01T00:00:00Z',
+        valid_time: '2024-07-01T00:00:00Z',
+        location_name: 'Rio de Janeiro',
+        overall_aqi_level: 1,
+      }),
+      createForecastAPIResponseData({
+        base_time: '2024-07-01T00:00:00Z',
+        valid_time: '2024-07-01T03:00:00Z',
+        location_name: 'Rio de Janeiro',
+        overall_aqi_level: 2,
+      }),
+      createForecastAPIResponseData({
+        base_time: '2024-07-01T00:00:00Z',
+        valid_time: '2024-07-01T06:00:00Z',
+        location_name: 'Rio de Janeiro',
+        overall_aqi_level: 3,
+      }),
+    ]
 
-    //AQI 2 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T01:00:00Z',
-      pm2_5: 20,
-      pm10: 40,
-      so2: 200,
-      no2: 90,
-      o3: 100,
-      site_name: 'Tijuca',
-    }),
-    //AQI 3 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T02:00:00Z',
-      pm2_5: 25,
-      pm10: 50,
-      so2: 350,
-      no2: 120,
-      o3: 130,
-      site_name: 'Tijuca',
-    }),
-    //AQI 4 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T03:00:00Z',
-      pm2_5: 50,
-      pm10: 100,
-      so2: 500,
-      no2: 230,
-      o3: 240,
-      site_name: 'Tijuca',
-    }),
-    //AQI 5 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T04:00:00Z',
-      pm2_5: 75,
-      pm10: 150,
-      so2: 750,
-      no2: 340,
-      o3: 380,
-      site_name: 'Tijuca',
-    }),
-    //AQI 6 values
-    createMeasurementsCityPageResponseData({
-      measurement_date: '2024-07-01T05:00:00Z',
-      pm2_5: 800,
-      pm10: 1200,
-      so2: 1250,
-      no2: 1000,
-      o3: 800,
-      site_name: 'Tijuca',
-    }),
-  ]
-  await setupPageWithMockData(page, [
-    {
-      endpointUrl: '*/**/air-pollutant/forecast*',
-      mockedAPIResponse: mockedForecastResponse,
-    },
-    {
-      endpointUrl: '*/**/air-pollutant/measurements*',
-      mockedAPIResponse: mockedMeasurementsCityPageResponse,
-    },
-  ])
-  await gotoPage(page, '/city/Rio%20de%20Janeiro')
-  await cityPage.waitForAllGraphsToBeVisible()
-  await waitForIdleNetwork(page, cityPage.aqiChart)
-  await cityPage.setBaseTime('01/07/2024 00:00')
-  const chartShot = await cityPage.captureChartScreenshot(cityPage.aqiChart)
-  await expect(chartShot).toMatchSnapshot('Midnight-AQI-3.png')
+    const mockedMeasurementsCityPageResponse = [
+      //Extreme boundary values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-06-30T23:29:00Z',
+        pm2_5: 800,
+        pm10: 1200,
+        so2: 1250,
+        no2: 1000,
+        o3: 800,
+        site_name: 'Tijuca',
+      }),
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T00:30:00Z',
+        pm2_5: 800,
+        pm10: 1200,
+        so2: 1250,
+        no2: 1000,
+        o3: 800,
+        site_name: 'Tijuca',
+      }),
+      //AQI 1 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T00:00:00Z',
+        pm2_5: 10,
+        pm10: 20,
+        so2: 100,
+        no2: 40,
+        o3: 50,
+        site_name: 'Tijuca',
+      }),
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T00:00:00Z',
+        pm2_5: 20,
+        pm10: 40,
+        so2: 200,
+        no2: 90,
+        o3: 100,
+        site_name: 'AQI 2 Site',
+      }),
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T00:00:00Z',
+        pm2_5: 25,
+        pm10: 50,
+        so2: 350,
+        no2: 120,
+        o3: 130,
+        site_name: 'AQI 3 site',
+      }),
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T00:00:00Z',
+        pm2_5: 50,
+        pm10: 100,
+        so2: 500,
+        no2: 230,
+        o3: 240,
+        site_name: 'AQI 4 site',
+      }),
+
+      //AQI 2 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T01:00:00Z',
+        pm2_5: 20,
+        pm10: 40,
+        so2: 200,
+        no2: 90,
+        o3: 100,
+        site_name: 'Tijuca',
+      }),
+      //AQI 3 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T02:00:00Z',
+        pm2_5: 25,
+        pm10: 50,
+        so2: 350,
+        no2: 120,
+        o3: 130,
+        site_name: 'Tijuca',
+      }),
+      //AQI 4 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T03:00:00Z',
+        pm2_5: 50,
+        pm10: 100,
+        so2: 500,
+        no2: 230,
+        o3: 240,
+        site_name: 'Tijuca',
+      }),
+      //AQI 5 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T04:00:00Z',
+        pm2_5: 75,
+        pm10: 150,
+        so2: 750,
+        no2: 340,
+        o3: 380,
+        site_name: 'Tijuca',
+      }),
+      //AQI 6 values
+      createMeasurementsCityPageResponseData({
+        measurement_date: '2024-07-01T05:00:00Z',
+        pm2_5: 800,
+        pm10: 1200,
+        so2: 1250,
+        no2: 1000,
+        o3: 800,
+        site_name: 'Tijuca',
+      }),
+    ]
+
+    await setupPageWithMockData(page, [
+      {
+        endpointUrl: '*/**/air-pollutant/forecast*',
+        mockedAPIResponse: mockedForecastResponse,
+      },
+      {
+        endpointUrl: '*/**/air-pollutant/measurements*',
+        mockedAPIResponse: mockedMeasurementsCityPageResponse,
+      },
+    ])
+    await gotoPage(page, '/city/Rio%20de%20Janeiro')
+    await cityPage.waitForAllGraphsToBeVisible()
+    await waitForIdleNetwork(page, cityPage.aqiChart)
+    await cityPage.setBaseTime('01/07/2024 00:00')
+    await banner.confirmDate()
+  })
+
+  test('PM2.5 creates an AQI 3 plot at 00:00', async ({ cityPage }) => {
+    const chartShot = await cityPage.captureChartScreenshot(cityPage.aqiChart)
+    await expect(chartShot).toMatchSnapshot('Midnight-AQI-3.png')
+  })
+
+  test('AQI 2 is plotted when AQI site 4 is removed', async ({ cityPage }) => {
+    await cityPage.siteRemover('AQI 4 site')
+    const chartShot = await cityPage.captureChartScreenshot(cityPage.aqiChart)
+    await expect(chartShot).toMatchSnapshot('Midnight-AQI-2.png')
+  })
 })
