cda64c597d6ecf76cfad7fd5c97b042cbe7d2fdb
Richard Strange <rstrange@scottlogic.com>
2024-07-31 13:55:24 +0100

    Remove the updated public dir which stops the logo being displayed correctly (#272)

--- air-quality-ui/vite.config.ts
+++ air-quality-ui/vite.config.ts
@@ -1,14 +1,7 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: path.resolve(
-    path.dirname(fileURLToPath(import.meta.url)),
-    'data_textures',
-  ),
 })
