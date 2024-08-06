93fc2e67a8975d9eafaf0f1c26a9a0276ac41102
Benjamin Ell-Jones <56438486+Ben-EJ@users.noreply.github.com>
2024-08-06 09:27:56 +0100

    Refactors complete

--- air-quality-backend/system_tests/api_suite/texture_api_validation_test.py
+++ air-quality-backend/system_tests/api_suite/texture_api_validation_test.py
@@ -11,43 +11,38 @@ from system_tests.utils.routes import Routes
 base_url = Routes.texture_api_endpoint
 headers = {"accept": "application/json"}
 
+textureChunkOne = {
+    "source": "cams-production",
+    "forecast_base_time": datetime.datetime(2024, 8, 4, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
+    "variable": "aqi",
+    "time_end": "2024-08-06T09:00:00.000Z",
+    "time_start": "2024-08-04T12:00:00.000Z",
+    "chunk": "1 of 3",
+    "last_modified_time": "2024-08-05T09:19:52.351Z",
+    "max_value": 7,
+    "min_value": 1,
+    "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_1_of_3.webp",
+    "units": "fractional overall AQI",
+    "created_time": "2024-08-05T09:19:52.351Z"
+}
 
-@pytest.fixture()
-def setup_test():
-    # Test Setup
-    load_dotenv()
-    delete_database_data("data_textures")
-    seed_api_test_data(
-        "data_textures",
-        [{
-            "source": "cams-production",
-            "forecast_base_time": datetime.datetime(2024, 8, 4, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
-            "variable": "aqi",
-            "time_end": "2024-08-06T09:00:00.000Z",
-            "time_start": "2024-08-04T12:00:00.000Z",
-            "chunk": "1 of 3",
-            "last_modified_time": "2024-08-05T09:19:52.351Z",
-            "max_value": 7,
-            "min_value": 1,
-            "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_1_of_3.webp",
-            "units": "fractional overall AQI",
-            "created_time": "2024-08-05T09:19:52.351Z"
-        },
-            {
-                "source": "cams-production",
-                "forecast_base_time": datetime.datetime(2024, 8, 4, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
-                "variable": "aqi",
-                "time_end": "2024-08-08T09:00:00.000Z",
-                "time_start": "2024-08-06T12:00:00.000Z",
-                "chunk": "2 of 3",
-                "last_modified_time": "2024-08-05T09:19:52.351Z",
-                "max_value": 7,
-                "min_value": 1,
-                "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_2_of_3.webp",
-                "units": "fractional overall AQI",
-                "created_time": "2024-08-05T09:19:52.351Z"
-            },
-            {
+textureChunkTwo = {
+
+    "source": "cams-production",
+    "forecast_base_time": datetime.datetime(2024, 8, 4, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
+    "variable": "aqi",
+    "time_end": "2024-08-08T09:00:00.000Z",
+    "time_start": "2024-08-06T12:00:00.000Z",
+    "chunk": "2 of 3",
+    "last_modified_time": "2024-08-05T09:19:52.351Z",
+    "max_value": 7,
+    "min_value": 1,
+    "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_2_of_3.webp",
+    "units": "fractional overall AQI",
+    "created_time": "2024-08-05T09:19:52.351Z"
+}
+
+textureChunkThree = {
                 "source": "cams-production",
                 "forecast_base_time": datetime.datetime(2024, 8, 4, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
                 "variable": "aqi",
@@ -60,12 +55,56 @@ def setup_test():
                 "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_3_of_3.webp",
                 "units": "fractional overall AQI",
                 "created_time": "2024-08-05T09:19:52.351Z"
-            },
-            {
+            }
+outOfRangeOfRequestDateBelow = {  # out of range of request date (below test date)
+                "source": "cams-production",
+                "forecast_base_time": datetime.datetime(2024, 8, 3, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
+                "variable": "aqi",
+                "time_end": "2024-08-09T12:00:00.000Z",
+                "time_start": "2024-08-08T12:00:00.000Z",
+                "chunk": "3 of 3",
+                "last_modified_time": "2024-08-05T09:19:52.351Z",
+                "max_value": 7,
+                "min_value": 1,
+                "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_3_of_3.webp",
+                "units": "fractional overall AQI",
+                "created_time": "2024-08-05T09:19:52.351Z"
+            }
+outOfRangeOfRequestDateAbove = {  # out of range of request date (above test date)
+                "source": "cams-production",
+                "forecast_base_time": datetime.datetime(2024, 8, 6, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
+                "variable": "aqi",
+                "time_end": "2024-08-09T12:00:00.000Z",
+                "time_start": "2024-08-08T12:00:00.000Z",
+                "chunk": "3 of 3",
+                "last_modified_time": "2024-08-05T09:19:52.351Z",
+                "max_value": 7,
+                "min_value": 1,
+                "texture_uri": "root/aqi_2024-08-04_12_CAMS_global.chunk_3_of_3.webp",
+                "units": "fractional overall AQI",
+                "created_time": "2024-08-05T09:19:52.351Z"
+            }
+invalidDatabaseEntry = {
                 "source": "cams-production",
                 "forecast_base_time": datetime.datetime(2024, 8, 5, 12, 00, 00, 00, tzinfo=datetime.timezone.utc),
                 "variable": True,
             }
+
+
+@pytest.fixture()
+def setup_test():
+    # Test Setup
+    load_dotenv()
+    delete_database_data("data_textures")
+    seed_api_test_data(
+        "data_textures",
+        [
+            textureChunkOne,
+            textureChunkTwo,
+            textureChunkThree,
+            outOfRangeOfRequestDateBelow,
+            outOfRangeOfRequestDateAbove,
+            invalidDatabaseEntry
         ]
     )
 
@@ -90,6 +129,9 @@ def test__required_parameters_provided__verify_response_status_200_and_data_retu
                 {"": ""}
         ),
         (
+                {}
+        ),
+        (
                 {"base_time": "2024-08-04T13:00:00.000Z "}
         ),
         (
@@ -106,7 +148,7 @@ def test__required_parameters_provided__verify_response_status_200_and_data_retu
         ),
     ],
 )
-def test__incorrect_dates__verify_response_status_404(setup_test, api_parameters: dict):
+def test__incorrect_dates__verify_response_status_422(setup_test, api_parameters: dict):
     response = requests.request(
         "GET", base_url, headers=headers, params=api_parameters, timeout=5.0
     )
