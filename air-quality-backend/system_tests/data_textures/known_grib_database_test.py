9dc86a5a796ea3d95c38427f648a387f70dc1922
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-08-08 17:04:01 +0100

    import update

--- air-quality-backend/system_tests/data_textures/known_grib_database_test.py
+++ air-quality-backend/system_tests/data_textures/known_grib_database_test.py
@@ -1,11 +1,10 @@
-import copy
-import random
-from datetime import timezone, datetime, timedelta
+import datetime
+
 import pytest
+from dotenv import load_dotenv
 
 from etl.scripts.run_forecast_etl import main
 import os
-from dotenv import load_dotenv
 from unittest import mock
 
 from system_tests.utils.database_utilities import (
@@ -14,8 +13,8 @@ from system_tests.utils.database_utilities import (
 )
 
 load_dotenv()
-forecast_base_time = datetime(2024, 6, 4, 0, 0, 0, tzinfo=timezone.utc)
-data_query = {"forecast_base_time": {"$eq": forecast_base_time}}
+forecast_base_time = datetime.datetime(2024, 6, 4, 0, 0, 0, tzinfo=datetime.timezone.utc)
+data_query = {"forecast_base_time": forecast_base_time}
 
 
 @pytest.fixture(scope="module")
@@ -23,14 +22,14 @@ def setup_data():
     # Set up code
     with mock.patch.dict(os.environ, {
         "FORECAST_BASE_TIME": "2024-6-4 00",
-        "FORECAST_RETRIEVAL_PERIOD": "0"
+        "FORECAST_RETRIEVAL_PERIOD": "1"
     }):
         delete_database_data("data_textures", data_query)
         main()
         yield
 
 
-def test_epic_test(setup_data):
+def test__epic_test(setup_data):
     dict_result = get_database_data("data_textures", data_query)
     expected_doc_count = 7 * 3
     assert (len(dict_result) == expected_doc_count), f"Expected {expected_doc_count} documents for one forecast_base_time"
