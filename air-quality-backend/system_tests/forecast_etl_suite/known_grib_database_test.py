5b27c3df1637c11ef3b9e5e56c51791a78083e43
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-08-09 16:31:49 +0100

    Fix test to also delete data from the forecast_data collection

--- air-quality-backend/system_tests/forecast_etl_suite/known_grib_database_test.py
+++ air-quality-backend/system_tests/forecast_etl_suite/known_grib_database_test.py
@@ -1,4 +1,5 @@
 import datetime
+import pprint
 
 import pytest
 from dotenv import load_dotenv
@@ -14,7 +15,8 @@ from system_tests.utils.database_utilities import (
 
 load_dotenv()
 forecast_base_time = datetime.datetime(2024, 6, 4, 0, 0, 0, tzinfo=datetime.timezone.utc)
-data_query = {"forecast_base_time": forecast_base_time}
+
+data_query = {"forecast_base_time": datetime.datetime(2024, 6, 4, 00, tzinfo=datetime.timezone.utc)}
 
 
 @pytest.fixture(scope="module")
@@ -22,16 +24,18 @@ def setup_data():
     # Set up code
     with mock.patch.dict(os.environ, {
         "FORECAST_BASE_TIME": "2024-6-4 00",
-        "FORECAST_RETRIEVAL_PERIOD": "1"
+        "FORECAST_RETRIEVAL_PERIOD": "0"
     }):
         delete_database_data("data_textures", data_query)
+        delete_database_data("forecast_data", data_query)
+
         main()
         yield
 
 
 def test__epic_test(setup_data):
     dict_result = get_database_data("data_textures", data_query)
+    pprint.pprint(dict_result)
     expected_doc_count = 7 * 3
-    assert (len(dict_result) == expected_doc_count), f"Expected {expected_doc_count} documents for one forecast_base_time"
-
-
+    assert (
+                len(dict_result) == expected_doc_count), f"Expected {expected_doc_count} documents for one forecast_base_time"
