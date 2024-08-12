f462c1a112a6bfd3173d6272dcdb4c17194261bd
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-08-12 09:45:44 +0100

    Update variable list and expectations

--- air-quality-backend/system_tests/data_textures/known_grib_database_test.py
+++ air-quality-backend/system_tests/data_textures/known_grib_database_test.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timezone, datetime, timedelta
 import pprint
 
 import pytest
@@ -39,3 +39,34 @@ def test__epic_test(setup_data):
     expected_doc_count = 7 * 3
     assert (
                 len(dict_result) == expected_doc_count), f"Expected {expected_doc_count} documents for one forecast_base_time"
+
+
+@pytest.mark.parametrize(
+    "variable, forecast_base_time, expected_units, expected_max_value, expected_min_value",
+    [
+        ("aqi",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "factional overall AQI", 7, 1
+         ),
+        ("winds_10m",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "m s**-1", 50, -50),
+        ("no2",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "kg m**-3 * 1e-9", 100, 0),
+        ("o3",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "kg m**-3 * 1e-9", 500, 0),
+        ("pm10",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "kg m**-3 * 1e-9", 1000, 0),
+        ("pm2_5",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "kg m**-3 * 1e-9", 1000, 0),
+        ("so2",
+         datetime(2024, 6, 4, 3, 0, 0, tzinfo=timezone.utc),
+         "kg m**-3 * 1e-9", 100, 0)
+    ]
+)
+def test__aqi_units(setup_data):
+    dict_result = get_database_data("data_textures")
