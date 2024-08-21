eb9bb79c68052e6fb81ac59c1d3976043d7d2282
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-08-21 17:32:21 +0100

    step definitions as ts

---
+++ air-quality-ui/system_tests/e2e/step_definitions/summary_table_step_defs.ts
@@ -0,0 +1,11 @@
+import { Given } from '@cucumber/cucumber'
+// import { Browser, Page, chromium } from '@playwright/test'
+
+// import { gotoPage } from '../../utils/helper_methods'
+
+Given('the user navigates to the summary page', async function () {
+  // const browser: Browser = await chromium.launch({ headless: false })
+  // const page: Page = await browser.newPage()
+  // await gotoPage(page, '/city/summary')
+  console.log('test')
+})
