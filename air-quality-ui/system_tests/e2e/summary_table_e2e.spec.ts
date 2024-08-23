1a0439037b534a522ea4311743eb5b5a99da9be9
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-08-23 17:29:25 +0100

    updated test data

--- air-quality-ui/system_tests/e2e/summary_table_e2e.spec.ts
+++ air-quality-ui/system_tests/e2e/summary_table_e2e.spec.ts
@@ -1,5 +1,6 @@
 import { test } from '../utils/fixtures'
 import { gotoPage } from '../utils/helper_methods'
+import { CaseAQI2, CaseAQI4 } from '../utils/test_enums'
 
 test('Changing the forecast base time on the summary page sets the correct data', async ({
   page,
@@ -10,7 +11,7 @@ test('Changing the forecast base time on the summary page sets the correct data'
   await gotoPage(page, 'summary')
   await summaryPage.waitForLoad()
 
-  // When they change the forecast base time to "03/07/2024" "12:00"
+  // When they change the forecast base time to "21/08/2024" "12:00"
 
   // await banner.calendarIcon.click()
   // await banner.findSpecificMonthAndYear('July', '2024'07)
@@ -18,40 +19,40 @@ test('Changing the forecast base time on the summary page sets the correct data'
   // await banner.datePickerTimeOption1200.click()
   // await banner.dateOkButton.click()
 
-  await banner.setBaseTime('03/07/2024 12:00')
+  await banner.setBaseTime('21/08/2024 12:00')
   await banner.dateOkButton.click()
   await summaryPage.waitForLoad()
 
-  // And they look at the entry in the table for "London"
-  await summaryPage.filterByCity('London')
+  // And they look at the entry in the table for "Atlanta"
+  await summaryPage.filterByCity('Atlanta')
 
   // Then the table data should be based on the data for "date" "time", with forecast window "1
   const expectedTableContents: string[][] = [
     [
       // AQI Level
-      CaseAQI6.aqiLevel.toString(), // Forecast
-      CaseAQI3.aqiLevel.toString(), // Measured
-      '+3', // Diff
+      CaseAQI4.aqiLevel.toString(), // Forecast
+      CaseAQI2.aqiLevel.toString(), // Measured
+      '+2', // Diff
       // pm2.5
-      CaseAQI6.pm2_5.toString(), // Forecast
-      CaseAQI3.pm2_5.toString(), // Measured
-      '08 Jul 06:00', // Time
+      '10.2', // Forecast
+      '8', // Measured
+      '21 Aug 12:00', // Time
       // pm10
-      CaseAQI6.pm10.toString(), // Forecast
-      CaseAQI3.pm10.toString(), // Measured
-      '08 Jul 06:00', // Time
+      '21.7', // Forecast
+      '13.2', // Measured
+      '22 Aug 00:00', // Time
       // no2
-      CaseAQI6.no2.toString(), // Forecast
-      CaseAQI3.no2.toString(), // Measured
-      '08 Jul 06:00', // Time
+      '5', // Forecast
+      '28.2', // Measured
+      '21 Aug 12:00', // Time
       // o3
-      CaseAQI6.o3.toString(), // Forecast
-      CaseAQI3.o3.toString(), // Measured
-      '08 Jul 06:00', // Time
+      '132.2', // Forecast
+      '89.7', // Measured
+      '21 Aug 21:00', // Time
       //so2
-      CaseAQI6.so2.toString(), // Forecast
-      CaseAQI3.so2.toString(), // Measured
-      '08 Jul 06:00', // Time
+      '2.8', // Forecast
+      '5.3', // Measured
+      '21 Aug 12:00', // Time
     ],
   ]
   await summaryPage.waitForLoad()
