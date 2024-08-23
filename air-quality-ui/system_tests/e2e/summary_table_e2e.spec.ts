1f7aecb7b1e6d2011a2277e70757ab42832fb01c
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-08-23 08:35:35 +0100

    framework for first e2e

--- air-quality-ui/system_tests/e2e/summary_table_e2e.spec.ts
+++ air-quality-ui/system_tests/e2e/summary_table_e2e.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '../utils/fixtures'
+import { test } from '../utils/fixtures'
 import { gotoPage } from '../utils/helper_methods'
 
 test('Changing the forecast base time on the summary page sets the correct data', async ({
@@ -21,4 +21,39 @@ test('Changing the forecast base time on the summary page sets the correct data'
   await banner.setBaseTime('03/07/2024 12:00')
   await banner.dateOkButton.click()
   await summaryPage.waitForLoad()
+
+  // And they look at the entry in the table for "London"
+  await summaryPage.filterByCity('London')
+
+  // Then the table data should be based on the data for "date" "time", with forecast window "1
+  const expectedTableContents: string[][] = [
+    [
+      // AQI Level
+      CaseAQI6.aqiLevel.toString(), // Forecast
+      CaseAQI3.aqiLevel.toString(), // Measured
+      '+3', // Diff
+      // pm2.5
+      CaseAQI6.pm2_5.toString(), // Forecast
+      CaseAQI3.pm2_5.toString(), // Measured
+      '08 Jul 06:00', // Time
+      // pm10
+      CaseAQI6.pm10.toString(), // Forecast
+      CaseAQI3.pm10.toString(), // Measured
+      '08 Jul 06:00', // Time
+      // no2
+      CaseAQI6.no2.toString(), // Forecast
+      CaseAQI3.no2.toString(), // Measured
+      '08 Jul 06:00', // Time
+      // o3
+      CaseAQI6.o3.toString(), // Forecast
+      CaseAQI3.o3.toString(), // Measured
+      '08 Jul 06:00', // Time
+      //so2
+      CaseAQI6.so2.toString(), // Forecast
+      CaseAQI3.so2.toString(), // Measured
+      '08 Jul 06:00', // Time
+    ],
+  ]
+  await summaryPage.waitForLoad()
+  await summaryPage.assertGridAttributes('values', expectedTableContents)
 })
