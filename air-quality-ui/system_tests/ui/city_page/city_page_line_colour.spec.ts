d3a80996198df0a9c731c6032e1a61df551f4a76
Mike Walker-Rose <mwalker-rose@scottlogic.com>
2024-08-27 15:09:25 +0100

    refactor out common variables

--- air-quality-ui/system_tests/ui/city_page/city_page_line_colour.spec.ts
+++ air-quality-ui/system_tests/ui/city_page/city_page_line_colour.spec.ts
@@ -13,13 +13,27 @@ import {
   CaseAQI6,
 } from '../../utils/test_enums'
 
-test.beforeEach(async ({ cityPage, page, banner }) => {
+test.beforeEach(async ({ cityPage, page }) => {
+  const locationLondon: string = 'London'
+  const testSite1: string = 'Test Site 1'
+  const testSite2: string = 'Test Site 2'
+  const testSite3: string = 'Test Site 3'
+  const testSite4: string = 'Test Site 4'
+  const testSite5: string = 'Test Site 5'
+  const testSite6: string = 'Test Site 6'
+  const testSite7: string = 'Test Site 7'
+  const testSite8: string = 'Test Site 8'
+  const testSite9: string = 'Test Site 9'
+  const testSite10: string = 'Test Site 10'
+  const testSite11: string = 'Test Site 11'
+  const date0000: string = '2024-08-27T00:00:00Z'
+  const date0300: string = '2024-08-27T03:00:00Z'
   const mockedForecastResponse = [
     //AQI 3
     createForecastAPIResponseData({
-      location_name: 'London',
-      base_time: '2024-08-27T00:00:00Z',
-      valid_time: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      base_time: date0000,
+      valid_time: date0000,
       overall_aqi_level: 3,
       no2: { aqi_level: CaseAQI3.aqiLevel, value: CaseAQI3.no2 },
       o3: { aqi_level: CaseAQI3.aqiLevel, value: CaseAQI3.o3 },
@@ -29,9 +43,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     //AQI 4
     createForecastAPIResponseData({
-      location_name: 'London',
-      base_time: '2024-08-27T00:00:00Z',
-      valid_time: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      base_time: date0000,
+      valid_time: date0300,
       overall_aqi_level: 4,
       no2: { aqi_level: CaseAQI4.aqiLevel, value: CaseAQI4.no2 },
       o3: { aqi_level: CaseAQI4.aqiLevel, value: CaseAQI4.o3 },
@@ -43,8 +57,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
   const mockedMeasurementsCityPageResponse = [
     // Site 1, AQI 1 -> 2
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 1',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite1,
+      measurement_date: date0000,
       no2: CaseAQI1.no2,
       o3: CaseAQI1.o3,
       so2: CaseAQI1.so2,
@@ -52,8 +67,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI1.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 1',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite1,
+      measurement_date: date0300,
       no2: CaseAQI2.no2,
       o3: CaseAQI2.o3,
       so2: CaseAQI2.so2,
@@ -62,8 +78,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 2, AQI 2 -> 3
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 2',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite2,
+      measurement_date: date0000,
       no2: CaseAQI3.no2,
       o3: CaseAQI3.o3,
       so2: CaseAQI3.so2,
@@ -71,8 +88,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI3.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 2',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite2,
+      measurement_date: date0300,
       no2: CaseAQI4.no2,
       o3: CaseAQI4.o3,
       so2: CaseAQI4.so2,
@@ -81,8 +99,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 3, AQI 3 -> 4
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 3',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite3,
+      measurement_date: date0000,
       no2: CaseAQI3.no2,
       o3: CaseAQI3.o3,
       so2: CaseAQI3.so2,
@@ -90,8 +109,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI3.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 3',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite3,
+      measurement_date: date0300,
       no2: CaseAQI4.no2,
       o3: CaseAQI4.o3,
       so2: CaseAQI4.so2,
@@ -100,8 +120,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 4, AQI 4 -> 5
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 4',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite4,
+      measurement_date: date0000,
       no2: CaseAQI4.no2,
       o3: CaseAQI4.o3,
       so2: CaseAQI4.so2,
@@ -109,8 +130,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI4.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 4',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite4,
+      measurement_date: date0300,
       no2: CaseAQI5.no2,
       o3: CaseAQI5.o3,
       so2: CaseAQI5.so2,
@@ -119,8 +141,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 5, AQI 5 -> 6
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 5',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite5,
+      measurement_date: date0000,
       no2: CaseAQI5.no2,
       o3: CaseAQI5.o3,
       so2: CaseAQI5.so2,
@@ -128,8 +151,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI5.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 5',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite5,
+      measurement_date: date0300,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -138,8 +162,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 6, AQI 6 -> 1
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 6',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite6,
+      measurement_date: date0000,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -147,8 +172,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 6',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite6,
+      measurement_date: date0300,
       no2: CaseAQI1.no2,
       o3: CaseAQI1.o3,
       so2: CaseAQI1.so2,
@@ -157,8 +183,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 7, AQI 6 -> 2
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 7',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: 'London',
+      site_name: testSite7,
+      measurement_date: date0000,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -166,8 +193,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 7',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite7,
+      measurement_date: date0300,
       no2: CaseAQI2.no2,
       o3: CaseAQI2.o3,
       so2: CaseAQI2.so2,
@@ -176,7 +204,8 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 8, AQI 6 -> 3
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 8',
+      location_name: locationLondon,
+      site_name: testSite8,
       measurement_date: '2024-08-27T00:00:00Z',
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
@@ -185,8 +214,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 8',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite8,
+      measurement_date: date0300,
       no2: CaseAQI3.no2,
       o3: CaseAQI3.o3,
       so2: CaseAQI3.so2,
@@ -195,8 +225,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 9, AQI 6 -> 4
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 9',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite9,
+      measurement_date: date0000,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -204,8 +235,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 9',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite9,
+      measurement_date: date0300,
       no2: CaseAQI4.no2,
       o3: CaseAQI4.o3,
       so2: CaseAQI4.so2,
@@ -214,8 +246,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 10, AQI 6 -> 5
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 10',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite10,
+      measurement_date: date0000,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -223,8 +256,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 10',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite10,
+      measurement_date: date0300,
       no2: CaseAQI5.no2,
       o3: CaseAQI5.o3,
       so2: CaseAQI5.so2,
@@ -233,8 +267,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
     }),
     // Site 11, AQI 6 -> 6
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 11',
-      measurement_date: '2024-08-27T00:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite11,
+      measurement_date: date0000,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -242,8 +277,9 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       pm10: CaseAQI6.pm10,
     }),
     createMeasurementsCityPageResponseData({
-      site_name: 'Test Site 11',
-      measurement_date: '2024-08-27T03:00:00Z',
+      location_name: locationLondon,
+      site_name: testSite11,
+      measurement_date: date0300,
       no2: CaseAQI6.no2,
       o3: CaseAQI6.o3,
       so2: CaseAQI6.so2,
@@ -262,8 +298,6 @@ test.beforeEach(async ({ cityPage, page, banner }) => {
       mockedAPIResponse: mockedMeasurementsCityPageResponse,
     },
   ])
-  await gotoPage(page, '/city/Rio%20de%20Janeiro')
+  await gotoPage(page, '/city/London')
   await cityPage.waitForAllGraphsToBeVisible()
-  await cityPage.setBaseTime('01/07/2024 00:00')
-  await banner.confirmDate()
 })
