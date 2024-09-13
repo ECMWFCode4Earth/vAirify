b24f61a2888a6ef77080921c929cda613a9caad2
sebsteinig <seb.steinig@gmail.com>
2024-09-13 10:12:09 +0100

    jest mocking

--- air-quality-ui/jest.config.ts
+++ air-quality-ui/jest.config.ts
@@ -31,7 +31,7 @@ export default {
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
-    '\\.glsl$': '<rootDir>/test/__mocks__/glslMock.js',
+    "^.+.(vert|frag|glsl)$": "jest-transform-stub"
   },
   testPathIgnorePatterns: ['/node_modules/', '/system_tests/'],
   setupFiles: ['./jest.setup.ts'],
