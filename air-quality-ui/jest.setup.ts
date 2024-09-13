c5bf628cf4cc2fdf998842aa0b1914ab52f4f37a
sebsteinig <seb.steinig@gmail.com>
2024-09-13 09:05:38 +0100

    try to mock WebGL environment for Jest tests

--- air-quality-ui/jest.setup.ts
+++ air-quality-ui/jest.setup.ts
@@ -9,3 +9,28 @@ Object.defineProperty(global.self, 'crypto', {
     subtle: crypto.webcrypto.subtle,
   },
 })
+
+// moke three.js and react-three-fiber components which rely on WebGL
+jest.mock('@react-three/fiber', () => {
+  const ReactThreeFiber = jest.requireActual('@react-three/fiber');
+
+  return {
+    ...ReactThreeFiber,
+    Canvas: ({ children }) => <div>{children}</div>, // Mocking the Canvas
+  };
+});
+
+jest.mock('@react-three/drei', () => {
+  return {
+    CameraControls: () => <div />, // Mocking CameraControls component
+    // Add more mocks as needed
+  };
+});
+
+window.HTMLCanvasElement.prototype.getContext = () => {
+  return {
+    fillStyle: jest.fn(),
+    fillRect: jest.fn(),
+    // Add more mocked methods if needed
+  };
+};
