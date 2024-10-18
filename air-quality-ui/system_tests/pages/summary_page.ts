5db8119a5d2876cfe5399a783be9a135b8d858fe
Maxwell Nyamunda <142179888+mnyamunda-scottlogic@users.noreply.github.com>
2024-10-18 16:50:49 +0100

    specify which checkbox to click out of the two (#347)
    Co-authored-by: Sebastian Steinig <80361118+sebsteinig@users.noreply.github.com>

--- air-quality-ui/system_tests/pages/summary_page.ts
+++ air-quality-ui/system_tests/pages/summary_page.ts
@@ -22,11 +22,15 @@ export class SummaryPage extends BasePage {
     this.agCell = page.locator('role=gridcell')
     this.allCells = page.locator('[role=gridcell]')
     this.grid = page.getByTestId('summary-grid')
-    this.highlightValuesToggle = page.getByRole('checkbox')
+    this.highlightValuesToggle = page
+      .locator('div')
+      .filter({ hasText: /^Highlight all AQI values$/ })
+      .getByRole('checkbox')
     this.locationFilterIcon = page.locator(
       '//span[contains(@class, "ag-icon-menu")]',
     )
     this.locationFilterTextBox = page.getByLabel('Filter Value')
+
     this.scroller = page.locator('.ag-body-horizontal-scroll-viewport')
     this.timeRange = page.getByText('Time Range:')
     this.title = page.locator('title')
