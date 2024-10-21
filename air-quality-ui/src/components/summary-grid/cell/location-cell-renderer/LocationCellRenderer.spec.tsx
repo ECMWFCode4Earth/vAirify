603db890304a2538e8e3e341b6312746877e856c
Chris Wilton-Magras <chriswilty@zoho.com>
2024-10-21 16:50:38 +0100

    Update LocationCellRenderer component tests

--- air-quality-ui/src/components/summary-grid/cell/location-cell-renderer/LocationCellRenderer.spec.tsx
+++ air-quality-ui/src/components/summary-grid/cell/location-cell-renderer/LocationCellRenderer.spec.tsx
@@ -1,15 +1,36 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
 
 import { LocationCellRenderer } from './LocationCellRenderer'
 
 describe('LocationCellRenderer component', () => {
-  it('renders the location', () => {
-    render(<LocationCellRenderer value={'Bristol'} />, {
+  const city = 'Bristol'
+  const eGridCell = document.createElement('div')
+
+  const renderComponent = () => {
+    const user = userEvent.setup()
+    render(<LocationCellRenderer value={city} eGridCell={eGridCell} />, {
       wrapper: BrowserRouter,
     })
-    expect(screen.getByRole('link')).toHaveTextContent('Bristol')
-    expect(screen.getByRole('link')).toHaveAttribute('href', '/city/Bristol')
+    return { user }
+  }
+
+  it('renders the location', () => {
+    renderComponent()
+    expect(screen.getByRole('link')).toHaveTextContent(city)
+  })
+
+  it('Navigates to single city page on click', async () => {
+    const { user } = renderComponent()
+    await user.click(screen.getByRole('link'))
+    expect(document.location.pathname).toBe(`/city/${city}`)
+  })
+
+  it('Navigates to single city page on typing Enter', async () => {
+    const { user } = renderComponent()
+    await user.type(eGridCell, '[Enter]')
+    expect(document.location.pathname).toBe(`/city/${city}`)
   })
 })
