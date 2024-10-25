e7747ce8dc2e86b5dc7370463f5e528cfdd9b024
Chris Wilton-Magras <chriswilty@zoho.com>
2024-10-25 09:19:38 +0100

    Accessibility fixes: keyboard navigation to city, and checkbox labels (#351)
    * fix: Enter key on city gridcell fires navigation
    
    * fix: Checkbox must be associated with label using id
    
    * Remove unused react icon :)
    
    * Add xmind as binary to gitattributes
    
    * Update LocationCellRenderer component tests
    
    * Correct keyboard listener test

--- air-quality-ui/src/components/summary-grid/cell/location-cell-renderer/LocationCellRenderer.spec.tsx
+++ air-quality-ui/src/components/summary-grid/cell/location-cell-renderer/LocationCellRenderer.spec.tsx
@@ -1,15 +1,62 @@
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import { BrowserRouter } from 'react-router-dom'
 
 import { LocationCellRenderer } from './LocationCellRenderer'
 
+const mockAddEventListener = jest.fn()
+
 describe('LocationCellRenderer component', () => {
+  const eGridCell: Partial<HTMLElement> = {
+    addEventListener: mockAddEventListener,
+    setAttribute: () => {},
+    removeEventListener: () => {},
+  }
+
+  const renderComponent = (city: string) =>
+    render(
+      <LocationCellRenderer
+        value={city}
+        eGridCell={eGridCell as HTMLElement}
+      />,
+      {
+        wrapper: BrowserRouter,
+      },
+    )
+
+  afterEach(() => {
+    mockAddEventListener.mockClear()
+  })
+
   it('renders the location', () => {
-    render(<LocationCellRenderer value={'Bristol'} />, {
-      wrapper: BrowserRouter,
+    const city = 'London'
+    renderComponent(city)
+    expect(screen.getByRole('link')).toHaveTextContent(city)
+  })
+
+  it('Navigates to single city page on click', async () => {
+    const city = 'Paris'
+    const user = userEvent.setup()
+    renderComponent(city)
+    await user.click(screen.getByRole('link'))
+    expect(window.location.pathname).toBe(`/city/${city}`)
+  })
+
+  it('Binds keyboard event handler to eGridCell', async () => {
+    // Cannot test actual interaction because eGridCell is not added to the DOM
+    // by LocationCellRenderer, so all we can do is test calls
+    const city = 'Bristol'
+    renderComponent(city)
+
+    expect(mockAddEventListener).toHaveBeenCalledTimes(1)
+    const [eventName, eventCallback] = mockAddEventListener.mock
+      .calls[0] as Parameters<HTMLElement['addEventListener']>
+    expect(eventName).toEqual('keydown')
+
+    act(() => {
+      ;(eventCallback as EventListener)({ code: 'Enter' } as KeyboardEvent)
     })
-    expect(screen.getByRole('link')).toHaveTextContent('Bristol')
-    expect(screen.getByRole('link')).toHaveAttribute('href', '/city/Bristol')
+    expect(window.location.pathname).toBe(`/city/${city}`)
   })
 })
