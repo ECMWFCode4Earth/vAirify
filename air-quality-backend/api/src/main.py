2246714e18ed80e5799f85486eb00d9faabee9a9
sebsteinig <seb.steinig@gmail.com>
2026-03-26 11:59:36 +0100

    fixed package versions

--- air-quality-backend/api/src/main.py
+++ air-quality-backend/api/src/main.py
@@ -6,7 +6,9 @@ from . import forecast_controller, measurements_controller, texture_controller
 
 origins = [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
